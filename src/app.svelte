db1dcb6ef449ec09f62718200923ede37ef9afb4
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2022-12-26 19:09:04 +0800

    internal: make a sample usage

--- src/app.svelte
+++ src/app.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+	import "agnostic-svelte/css/common.min.css"
+	import { Button } from "agnostic-svelte"
 	export let name: string
 </script>
 
 <p>
 	Hello {name}!
 </p>
+<Button mode="primary">Sample button</Button>
 
 <style lang="scss">
 	p {
