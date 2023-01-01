0b83395c0a6b2ad9399b370c658c60f8d09c7ea6
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-01 20:12:00 +0800

    style: customize the themes

--- tailwindcss.config.js
+++ tailwindcss.config.js
@@ -1,13 +1,31 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
 	"content": ["./src/pages/**/*.svelte", "./src/components/**/*.svelte"],
-	"darkMode": "dark",
+	"darkMode": "[data-them='dark-logo']",
 	"theme": {
-		"extend": {
-			"primary": "#220055",
-			"secondary": "#e5d5ff",
-			"accent": "#808080"
-		},
+		"extend": {}
+	},
+	"daisyui": {
+		"themes": [
+			{
+				"light-logo": {
+					"primary": "#e5d5ff",
+					"secondary": "#220055",
+					"accent": "#808080",
+					"neutral": "#191D24",
+					"base-100": "#2A303C"
+				}
+			},
+			{
+				"dark-logo": {
+					"primary": "#220055",
+					"secondary": "#e5d5ff",
+					"accent": "#808080",
+					"neutral": "#191D24",
+					"base-100": "#2A303C"
+				}
+			}
+		]
 	},
 	"variants": {},
 	"plugins": [
