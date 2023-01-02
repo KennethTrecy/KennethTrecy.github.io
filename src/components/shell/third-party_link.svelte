10f33a5d5bfb2a68f73222b7ba7286c5475d0388
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-02 12:48:34 +0800

    internal: make third-party link component

---
+++ src/components/shell/third-party_link.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import UnendorsedLink from "@/components/general/unendorsed_link.svelte"
+
+	export let packageName
+	export let homeLink
+	export let licenseName
+	export let licenseLink
+</script>
+
+<UnendorsedLink address={homeLink}>{packageName}</UnendorsedLink>
+(<UnendorsedLink address={licenseLink}>{licenseName}</UnendorsedLink>)
