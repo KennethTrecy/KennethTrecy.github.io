4e0003c480c942c2cf164637b2bf3549e2dddbed
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-08 22:04:52 +0800

    internal: redo the mechanism of shell

--- src/components/shell.svelte
+++ src/components/shell.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	import "@/components/general.css"
-	import { mustBeInDarkMode, themeName } from "@/components/general/theme"
 	import Logo from "@/multimedia/logo.png"
+
+	import Icon from "@/components/general/icon"
 	import Menu from "@/components/shell/menu.svelte"
 	import ProfileLink from "@/components/shell/profile_link.svelte"
+	import ThemeToggler from "@/components/shell/theme_toggler.svelte"
 	import ThirdPartyLink from "@/components/shell/third-party_link.svelte"
-
-	$: document.documentElement.setAttribute("data-theme", $themeName)
-	$: friendlyCurrentMode = $mustBeInDarkMode ? "Dark" : "Light"
 </script>
 
 <svelte:head>
@@ -22,88 +21,101 @@
 			font-family: "Roboto", sans-serif;
 		}
 	</style>
+	<meta name="refresh" content="5"/>
 </svelte:head>
-<header class="navbar">
-	<a href="/" class="text-color-inherit navbar-start flex flex-row items-center">
-		<img class="flex-none w-8 h-auto" alt="logo" src={Logo}/>
-		<p class="flex-1 ml-4 my-auto">
-			KennethTrecy
-		</p>
-	</a>
-	<nav class="navbar-end">
-		<div class="block md:hidden">
-			<div class="dropdown dropdown-end">
-				<label tabindex="0" for="navbar_dropdown" class="material-symbols-outlined">menu</label>
-				<div tabindex="0" id="navbar_dropdown" class="dropdown-content">
-					<Menu/>
-				</div>
-			</div>
-		</div>
-		<div class="hidden md:block">
-			<Menu mustShowHorizontally={true}/>
-		</div>
-	</nav>
-</header>
-<main>
-	<slot name="main"></slot>
-</main>
-<footer class="footer footer-center p-4">
-	<div>
-		<p>
-			This website contains third-party packages:
-			<ThirdPartyLink
-				packageName="Svelte"
-				homeLink="https://svelte.dev/"
-				licenseName="MIT"
-				licenseLink="https://github.com/sveltejs/svelte/blob/master/LICENSE.md"/>,
-			<ThirdPartyLink
-				packageName="Tailwind CSS"
-				homeLink="https://tailwindcss.com/"
-				licenseName="MIT"
-				licenseLink="https://github.com/tailwindlabs/tailwindcss/blob/master/LICENSE"/>,
-			<ThirdPartyLink
-				packageName="daisyUI"
-				homeLink="https://daisyui.com/"
-				licenseName="MIT"
-				licenseLink="https://github.com/saadeghi/daisyui/blob/master/LICENSE"/>, and
-			<ThirdPartyLink
-				packageName="rgb-hex"
-				homeLink="https://github.com/sindresorhus/rgb-hex#readme"
-				licenseName="MIT"
-				licenseLink="https://github.com/sindresorhus/rgb-hex/blob/main/license"/>.
-		</p>
-	</div>
-</footer>
-<footer class="footer justify-around items-center p-4 bg-base-200">
-	<section class="md:grid-flow-col md:items-center">
-		<img class="flex-none w-16 h-auto" alt="logo" src={Logo}/>
-		<p>Copyright © 2023 Kenneth Trecy Tobias</p>
-	</section>
-	<section class="socials justify-center items-center justify-items-center">
-		<span class="footer-title">Socials</span>
-		<p class="grid grid-cols-2 grid-rows-1 gap-x-4">
-			<ProfileLink
-				address="https://www.linkedin.com/in/kenneth-trecy-tobias-4341a825b/"
-				icon="linkedin"
-				name="LinkedIn"/>
-			<ProfileLink
-				address="https://github.com/KennethTrecy/"
-				icon="github"
-				name="GitHub"/>
-		</p>
-	</section>
-	<section class="md:justify-self-end">
-		<div class="form-control">
-			<label class="label cursor-pointer">
-				<span class="label-text">Current mode: {friendlyCurrentMode}</span>
-				<input
-					type="checkbox"
-					class="toggle toggle-secondary ml-4"
-					bind:checked={$mustBeInDarkMode}/>
+<div class="drawer drawer-mobile">
+	<input type="checkbox" class="drawer-toggle" id="menu_drawer_checkbox"/>
+	<div class="drawer-content">
+		<header class="navbar">
+			<label
+				tabindex="0"
+				for="menu_drawer_checkbox"
+				class="drawer-button btn bg-transparent border-transparent lg:hidden text-secondary hover:text-primary hover:bg-secondary">
+				<Icon name="menu"/>
 			</label>
-		</div>
-	</section>
-</footer>
+			<nav class="navbar-center">
+				<a href="/" class="text-color-inherit flex flex-row items-start">
+					<img class="flex-none w-16 h-auto" alt="logo" src={Logo}/>
+					<p class="flex-1 ml-2 my-auto">
+						KennethTrecy
+					</p>
+				</a>
+			</nav>
+			<div class="flex-1 flex flex-row justify-end items-center">
+				<ThemeToggler/>
+			</div>
+		</header>
+		<main>
+			<slot name="main"></slot>
+		</main>
+		<footer class="footer footer-center p-4">
+			<div>
+				<p>
+					This website contains third-party packages:
+					<ThirdPartyLink
+						packageName="Svelte"
+						homeLink="https://svelte.dev/"
+						licenseName="MIT"
+						licenseLink="https://github.com/sveltejs/svelte/blob/master/LICENSE.md"/>,
+					<ThirdPartyLink
+						packageName="Tailwind CSS"
+						homeLink="https://tailwindcss.com/"
+						licenseName="MIT"
+						licenseLink="https://github.com/tailwindlabs/tailwindcss/blob/master/LICENSE"/>,
+					<ThirdPartyLink
+						packageName="@tailwindcss/typography"
+						homeLink="https://github.com/tailwindlabs/tailwindcss-typography#readme"
+						licenseName="MIT"
+						licenseLink="https://github.com/tailwindlabs/tailwindcss-typography/blob/master/LICENSE"/>,
+					<ThirdPartyLink
+						packageName="daisyUI"
+						homeLink="https://daisyui.com/"
+						licenseName="MIT"
+						licenseLink="https://github.com/saadeghi/daisyui/blob/master/LICENSE"/>, and
+					<ThirdPartyLink
+						packageName="rgb-hex"
+						homeLink="https://github.com/sindresorhus/rgb-hex#readme"
+						licenseName="MIT"
+						licenseLink="https://github.com/sindresorhus/rgb-hex/blob/main/license"/>.
+				</p>
+			</div>
+		</footer>
+		<footer class="footer justify-around items-center p-4 bg-base-200">
+			<section class="md:grid-flow-col md:items-center">
+				<img class="flex-none w-16 h-auto" alt="logo" src={Logo}/>
+				<p>Copyright © 2023 Kenneth Trecy Tobias</p>
+			</section>
+			<section class="socials justify-center items-center justify-items-center">
+				<span class="footer-title">Socials</span>
+				<p class="grid grid-cols-2 grid-rows-1 gap-x-4">
+					<ProfileLink
+						address="https://www.linkedin.com/in/kenneth-trecy-tobias-4341a825b/"
+						icon="linkedin"
+						name="LinkedIn"/>
+					<ProfileLink
+						address="https://github.com/KennethTrecy/"
+						icon="github"
+						name="GitHub"/>
+				</p>
+			</section>
+		</footer>
+	</div>
+	<div tabindex="0" class="drawer-side">
+		<label for="menu_drawer_checkbox" class="drawer-overlay"></label>
+		<aside class="w-80 bg-primary">
+			<a href="/" class="mb-4 border-b p-4 text-color-inherit flex flex-col items-start">
+				<img class="flex-none w-full h-auto" alt="logo" src={Logo}/>
+				<p class="flex-1 my-auto text-2xl">
+					KennethTrecy
+				</p>
+				<p class="prose">
+					An open-source technology enthusiast, back-end experimentalist, and analytic tester.
+				</p>
+			</a>
+			<Menu/>
+		</aside>
+	</div>
+</div>
 
 <style lang="postcss">
 	:root {
@@ -119,7 +131,6 @@
 
 	main {
 		min-height: var(--main-height);
-		height: var(--main-height);
 	}
 
 	footer .socials {
---
+++ src/components/shell/theme_toggler.svelte
@@ -0,0 +1,16 @@
+<script lang="typescript">
+	import { mustBeInDarkMode, themeName } from "@/components/general/theme"
+
+	import Icon from "@/components/general/icon"
+
+	$: document.documentElement.setAttribute("data-theme", $themeName)
+	$: modeIcon = $mustBeInDarkMode ? "dark_mode" : "light_mode"
+</script>
+
+<label class="btn bg-transparent border-transparent cursor-pointer text-secondary hover:text-primary hover:bg-secondary">
+	<Icon name={modeIcon}/>
+	<input
+		type="checkbox"
+		class="hidden"
+		bind:checked={$mustBeInDarkMode}/>
+</label>
