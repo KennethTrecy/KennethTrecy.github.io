df63892dbbe18a4e3d35406c76ca9df33478654c
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-08 23:28:47 +0800

    build(production): dedicate the pipeline for production

--- bundler/prod/pipeline.js
+++ bundler/prod/pipeline.js
@@ -1,14 +1,22 @@
+import { readFile } from "node:fs/promises"
+import { join } from "node:path"
+
 import { PRODUCTION } from "../core/environments"
 
 import PluginArray from "./plugin_array"
-import DevPipeline from "../dev/pipeline"
+import CorePipeline from "../core/pipeline"
+import SourceDirectory from "./source_directory"
 
-export default class Pipeline extends DevPipeline {
+export default class Pipeline extends CorePipeline {
 	constructor() {
-		super(PRODUCTION)
+		super(PRODUCTION, "src/pages", "hidden/renderers", "cjs")
 	}
 
-	get pluginArrayClass() {
-		return PluginArray
+	async toConfigurationArray() {
+		return new SourceDirectory(
+			this.commonInfo,
+			pathPair => new PluginArray(this._environment, pathPair).rawArray,
+			[]
+		).toConfigurationArray()
 	}
 }
