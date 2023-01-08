0ea039f01634cc17ca3c9b2d19066f4d171a6a17
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-08 22:06:45 +0800

    internal: add missing page configuration

---
+++ src/pages/index.js
@@ -0,0 +1,36 @@
+const title = "KennethTrecy"
+const name = "Kenneth Trecy Tobias"
+
+module.exports = {
+	"title": title,
+	"meta": [
+		{
+			"name": "description",
+			"content": "Personal website of KennethTrecy"
+		},
+		{
+			"name": "keywords",
+			"content": "Kenneth Trecy,portfolio,philippines"
+		},
+		{
+			"name": "web_author",
+			"content": name
+		},
+		{
+			"name": "designer",
+			"content": name
+		},
+		{
+			"name": "creator",
+			"content": name
+		},
+		{
+			"name": "page-version",
+			"content": "0.1"
+		},
+		{
+			"name": "license",
+			"content": "MIT"
+		}
+	]
+}
