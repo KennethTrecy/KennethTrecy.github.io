37e103aea5f49d2caf0dc67704f5054b0a1906ce
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-14 01:17:09 +0800

    chore: rename the file extension of configuration for styles

--- tailwindcss.config.cjs
+++ tailwindcss.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
-	"content": ["./src/pages/**/*.svelte", "./src/components/**/*.svelte"],
+	"content": [
+		"./src/pages/**/*.svelte",
+		"./src/components/**/*.svelte",
+		"./src/routes/**/*.svelte"
+	],
 	"darkMode": "[data-them='dark-logo']",
 	"theme": {
 		"extend": {
