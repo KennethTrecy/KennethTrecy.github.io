2608a6d7c864b65cd3801908a9dcc92bf6753117
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-21 02:58:27 +0800

    internal(component): make a contact link

---
+++ src/components/about/contact_link.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import BrandIcon from "@/components/general/brand_icon.svelte"
+
+	export let address: string
+	export let icon: string
+	export let platformName: string
+	export let username: string
+</script>
+
+<a title={platformName} href={address} rel="me">
+	<BrandIcon {icon} alternateName={platformName}/>
+	<span>{username}</span>
+</a>
