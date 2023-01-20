f9d35b5cb40cd8432fed150888423512d317e37a
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-21 01:09:44 +0800

    style(component): change the layout of the card

--- src/components/general/project_card.svelte
+++ src/components/general/project_card.svelte
@@ -8,16 +8,12 @@
 
 <div class="card bg-base-200">
 	<div class="card-body">
-		<h4 class="card-title prose">{title}</h4>
+		<a class="card-title" href={link}>
+			<h4 class="prose">{title}</h4>
+			<Icon name="chevron_right"/>
+		</a>
 		<p class="prose">
 			{description}
 		</p>
-		<div class="card-actions">
-			<a
-				href={link}
-				class="flex flex-row flex-nowrap items-center justify-end">
-				<span>Visit</span><Icon name="chevron_right"/>
-			</a>
-		</div>
 	</div>
 </div>
