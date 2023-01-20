fa0d3a1f96acccbf19b8f11761e8172accda6d0c
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-21 02:54:51 +0800

    internal: specify the types of props

--- src/components/shell/profile_link.svelte
+++ src/components/shell/profile_link.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import BrandIcon from "@/components/general/brand_icon.svelte"
 
-	export let address
-	export let icon
-	export let name
+	export let address: string
+	export let icon: string
+	export let name: string
 </script>
 
 <a title={name} href={address} rel="me">
