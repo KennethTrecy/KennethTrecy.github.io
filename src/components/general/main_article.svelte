e1c1f9ae7ec8af739d1291a6bbbef78907b71eed
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-21 22:09:42 +0800

    refactor(home): separate tags for main article

---
+++ src/components/general/main_article.svelte
@@ -0,0 +1,9 @@
+<div class="h-full w-full flex flex-col justify-center items-center">
+	<article
+		itemprop="mainEntity"
+		itemscope
+		itemtype="https://schema.org/WebPageElement"
+		class="pb-8">
+		<slot></slot>
+	</article>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 
 	import BaseLink from "@/components/general/links/base.svelte"
 	import CommonHead from "@/components/general/common_head.svelte"
+	import MainArticle from "@/components/general/main_article.svelte"
 	import ProjectCard from "@/components/general/project_card.svelte"
 	import ExternalLink from "@/components/general/links/external.svelte"
 </script>
@@ -17,83 +18,77 @@
 		pageVersion="0.1"/>
 </svelte:head>
 
-<div class="h-full w-full flex flex-col justify-center items-center">
-	<article
-		itemprop="mainEntity"
-		itemscope
-		itemtype="https://schema.org/WebPageElement"
-		class="pb-8">
-		<section itemprop="mainEntity" itemscope itemtype="https://schema.org/Person" class="hero">
-			<div class="hero-content flex flex-col md:flex-row">
-				<img itemprop="image" src={Logo} alt="Kenneth Trecy's logo" class="max-w-sm"/>
-				<div class="max-w-md text-center md:text-left">
-					<h1 itemprop="headline" class="text-6xl font-bold">
-						Hi, I am <span itemprop="givenName">Kenneth Trecy</span>!
-					</h1>
-					<p itemprop="knowsAbout" class="prose md:prose-lg py-6">
-						I usually develop the back-end of the applications I have handled. I ensure their quality though automated tests through CI.
-					</p>
-					<BaseLink
-						itemprop="mainEntityOfPage"
-						address="/about"
-						relationship={[ ...internalTypes, ...authorTypes ]}>
-						<span class="btn btn-primary">Know more about me</span>
-					</BaseLink>
-				</div>
-			</div>
-		</section>
-		<div class="divider"></div>
-		<section
-			itemprop="hasPart"
-			itemscope
-			itemtype="https://schema.org/WebContent"
-			class="container my-4 flex flex-col justify-center items-center">
-			<div class="prose md:prose-lg">
-				<h2 itemprop="headline">Specialty</h2>
-				<p itemprop="text">Web development is my expertise. I have tried different technologies too such as networking, Internet of Things (<abbr>IoT</abbr>), and game.</p>
-				<p>During my free time, I apply my skills on my personal projects.</p>
-
-				<h3 class="my-4">Some of my personal projects</h3>
-				<div class="max-w-prose flex flex-col flex-wrap">
-					<ProjectCard
-						title="Virdafils"
-						description="A driver for Laravel framework which use a database like file storage system."
-						link="https://github.com/KennethTrecy/virdafils"/>
-					<ProjectCard
-						title="Elomocato"
-						description="Custom cast classes to be used with Laravel framework."
-						link="https://github.com/KennethTrecy/elomocato"/>
-					<ProjectCard
-						title="Comroconbu"
-						description="Builder for common Rollup configurations."
-						link="https://github.com/KennethTrecy/comroconbu"/>
-				</div>
-				<p class="mt-4">
-					Other projects can be found on the
-					<ExternalLink address="https://github.com/KennethTrecy?tab=repositories">
-						list of my respositories
-					</ExternalLink>
-					in
-					<BaseLink
-						address="https://github.com/KennethTrecy"
-						relationship={[ ...externalTypes, ...authorTypes ]}
-						target="other">my GitHub profile</BaseLink>.
+<MainArticle>
+	<section itemprop="mainEntity" itemscope itemtype="https://schema.org/Person" class="hero">
+		<div class="hero-content flex flex-col md:flex-row">
+			<img itemprop="image" src={Logo} alt="Kenneth Trecy's logo" class="max-w-sm"/>
+			<div class="max-w-md text-center md:text-left">
+				<h1 itemprop="headline" class="text-6xl font-bold">
+					Hi, I am <span itemprop="givenName">Kenneth Trecy</span>!
+				</h1>
+				<p itemprop="knowsAbout" class="prose md:prose-lg py-6">
+					I usually develop the back-end of the applications I have handled. I ensure their quality though automated tests through CI.
 				</p>
+				<BaseLink
+					itemprop="mainEntityOfPage"
+					address="/about"
+					relationship={[ ...internalTypes, ...authorTypes ]}>
+					<span class="btn btn-primary">Know more about me</span>
+				</BaseLink>
 			</div>
-		</section>
-		<section
-			itemprop="hasPart"
-			itemscope
-			itemtype="https://schema.org/WebContent"
-			class="container my-4 flex flex-col justify-center items-center">
-			<div class="prose md:prose-lg">
-				<h2 itemprop="headline">Security in mind</h2>
-				<p itemprop="text">I also like reading articles about cybersecurity. For this reason, I build applications with security in mind as much as possible to be resilient from attacks.</p>
-				<p itemprop="text">I have also made my recent commits to be signed with my own cryptographic keys.</p>
+		</div>
+	</section>
+	<div class="divider"></div>
+	<section
+		itemprop="hasPart"
+		itemscope
+		itemtype="https://schema.org/WebContent"
+		class="container my-4 flex flex-col justify-center items-center">
+		<div class="prose md:prose-lg">
+			<h2 itemprop="headline">Specialty</h2>
+			<p itemprop="text">Web development is my expertise. I have tried different technologies too such as networking, Internet of Things (<abbr>IoT</abbr>), and game.</p>
+			<p>During my free time, I apply my skills on my personal projects.</p>
+
+			<h3 class="my-4">Some of my personal projects</h3>
+			<div class="max-w-prose flex flex-col flex-wrap">
+				<ProjectCard
+					title="Virdafils"
+					description="A driver for Laravel framework which use a database like file storage system."
+					link="https://github.com/KennethTrecy/virdafils"/>
+				<ProjectCard
+					title="Elomocato"
+					description="Custom cast classes to be used with Laravel framework."
+					link="https://github.com/KennethTrecy/elomocato"/>
+				<ProjectCard
+					title="Comroconbu"
+					description="Builder for common Rollup configurations."
+					link="https://github.com/KennethTrecy/comroconbu"/>
 			</div>
-		</section>
-	</article>
-</div>
+			<p class="mt-4">
+				Other projects can be found on the
+				<ExternalLink address="https://github.com/KennethTrecy?tab=repositories">
+					list of my respositories
+				</ExternalLink>
+				in
+				<BaseLink
+					address="https://github.com/KennethTrecy"
+					relationship={[ ...externalTypes, ...authorTypes ]}
+					target="other">my GitHub profile</BaseLink>.
+			</p>
+		</div>
+	</section>
+	<section
+		itemprop="hasPart"
+		itemscope
+		itemtype="https://schema.org/WebContent"
+		class="container my-4 flex flex-col justify-center items-center">
+		<div class="prose md:prose-lg">
+			<h2 itemprop="headline">Security in mind</h2>
+			<p itemprop="text">I also like reading articles about cybersecurity. For this reason, I build applications with security in mind as much as possible to be resilient from attacks.</p>
+			<p itemprop="text">I have also made my recent commits to be signed with my own cryptographic keys.</p>
+		</div>
+	</section>
+</MainArticle>
 
 <style lang="postcss">
 	@keyframes rotate_clockwise {
