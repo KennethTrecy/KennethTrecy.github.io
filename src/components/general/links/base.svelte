94613d4c9ea8b8a16eb139f30e3df89e40223cdc
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-21 21:52:24 +0800

    internal(component): allow adding property to base link

--- src/components/general/links/base.svelte
+++ src/components/general/links/base.svelte
@@ -4,18 +4,19 @@
 	export let address: string
 	export let context: AnchorTarget = "self"
 	export let relationship: AnchorLinkType|AnchorLinkType[]
-	export let title: string = ""
+	export let title: string|undefined = undefined
+	export let itemprop: string|undefined = undefined
 	let group: string[] = []
 
 	export { group as class }
 
-	$: specifiedTitle = title ? title : undefined
 	$: relationshipTypes = Array.isArray(relationship) ? relationship.sort().join(" ") : relationship
 	$: target = context === "self" ? "_self" : "_blank"
 </script>
 
 <a
-	title={specifiedTitle}
+	{title}
+	{itemprop}
 	href={address}
 	class={[ "link", ...group ].join(" ")}
 	rel={relationshipTypes} {target}>
