0ce24c6d301f9845b1054d2c2b29dffa507dd7e3
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-21 22:21:50 +0800

    internal(component): allow customization of item type of article

--- src/components/general/main_article.svelte
+++ src/components/general/main_article.svelte
@@ -1,8 +1,16 @@
-<div class="h-full w-full flex flex-col justify-center items-center">
+<script lang="ts">
+	export let itemtype: string = "https://schema.org/WebPageContent"
+</script>
+
+<div
+	itemprop="mainContentOfPage"
+	itemscope
+	itemtype="https://schema.org/WebPageElement"
+	class="h-full w-full flex flex-col justify-center items-center">
 	<article
 		itemprop="mainEntity"
 		itemscope
-		itemtype="https://schema.org/WebContent"
+		{itemtype}
 		class="pb-8">
 		<slot></slot>
 	</article>
