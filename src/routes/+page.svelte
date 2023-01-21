3cf4f66b7afd60ae50b8a77de3aac8859b352309
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-22 02:49:50 +0800

    style(home): readjust the spaces

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,75 +19,87 @@
 </svelte:head>
 
 <MainArticle>
-	<section itemprop="mainEntity" itemscope itemtype="https://schema.org/Person" class="hero">
-		<div class="hero-content flex flex-col md:flex-row">
-			<img itemprop="image" src={Logo} alt="Kenneth Trecy's logo" class="max-w-sm"/>
-			<div class="max-w-md text-center md:text-left">
-				<h1 itemprop="headline" class="text-6xl font-bold">
-					Hi, I am <span itemprop="givenName">Kenneth Trecy</span>!
-				</h1>
-				<p itemprop="knowsAbout" class="prose md:prose-lg py-6">
-					I usually develop the back-end of the applications I have handled. I ensure their quality though automated tests through CI.
-				</p>
-				<BaseLink
-					itemprop="mainEntityOfPage"
-					address="/about"
-					relationship={[ ...internalTypes, ...authorTypes ]}>
-					<span class="btn btn-primary">Know more about me</span>
-				</BaseLink>
-			</div>
+	<h1 slot="title">KennethTrecy's Portfolio</h1>
+	<svelte:fragment slot="content">
+		<div class="divider"></div>
+		<div
+			itemprop="mainEntity"
+			itemscope
+			itemtype="https://schema.org/Person"
+			class="hero w-screen">
+			<section class="hero-content flex flex-col md:flex-row">
+				<img itemprop="image" src={Logo} alt="Kenneth Trecy's logo" class="max-w-sm"/>
+				<div class="max-w-md text-center md:text-left">
+					<h2 itemprop="headline">
+						Hi, I am <span itemprop="givenName">Kenneth Trecy</span>!
+					</h2>
+					<p itemprop="knowsAbout" class="py-6">
+						I usually develop the back-end of the applications I have handled. I ensure their quality though automated tests through CI.
+					</p>
+					<BaseLink
+						itemprop="mainEntityOfPage"
+						address="/about"
+						relationship={[ ...internalTypes, ...authorTypes ]}>
+						<span class="btn btn-primary">Know more about me</span>
+					</BaseLink>
+				</div>
+			</section>
 		</div>
-	</section>
-	<div class="divider"></div>
-	<section
-		itemprop="hasPart"
-		itemscope
-		itemtype="https://schema.org/WebContent"
-		class="container my-4 flex flex-col justify-center items-center">
-		<div class="prose md:prose-lg">
-			<h2 itemprop="headline">Specialty</h2>
-			<p itemprop="text">Web development is my expertise. I have tried different technologies too such as networking, Internet of Things (<abbr>IoT</abbr>), and game.</p>
-			<p>During my free time, I apply my skills on my personal projects.</p>
+		<div class="divider"></div>
+		<section
+			itemprop="hasPart"
+			itemscope
+			itemtype="https://schema.org/WebContent"
+			class="flex flex-col justify-center items-center">
+			<div class="container max-w-prose md:px-0">
+				<h2 itemprop="headline">Specialty</h2>
+				<p itemprop="text">Web development is my expertise. I have tried different technologies too such as networking, Internet of Things (<abbr>IoT</abbr>), and game.</p>
+				<p>During my free time, I apply my skills on my personal projects.</p>
 
-			<h3 class="my-4">Some of my personal projects</h3>
-			<div class="max-w-prose flex flex-col flex-wrap">
-				<ProjectCard
-					title="Virdafils"
-					description="A driver for Laravel framework which use a database like file storage system."
-					link="https://github.com/KennethTrecy/virdafils"/>
-				<ProjectCard
-					title="Elomocato"
-					description="Custom cast classes to be used with Laravel framework."
-					link="https://github.com/KennethTrecy/elomocato"/>
-				<ProjectCard
-					title="Comroconbu"
-					description="Builder for common Rollup configurations."
-					link="https://github.com/KennethTrecy/comroconbu"/>
+				<h3 class="my-4">Some of my personal projects</h3>
+				<div class="flex flex-col flex-wrap">
+					<ProjectCard
+						title="Virdafils"
+						description="A driver for Laravel framework which use a database like file storage system."
+						link="https://github.com/KennethTrecy/virdafils"/>
+					<ProjectCard
+						title="Elomocato"
+						description="Custom cast classes to be used with Laravel framework."
+						link="https://github.com/KennethTrecy/elomocato"/>
+					<ProjectCard
+						title="Comroconbu"
+						description="Builder for common Rollup configurations."
+						link="https://github.com/KennethTrecy/comroconbu"/>
+				</div>
+				<p class="mt-4">
+					Other projects can be found on the
+					<ExternalLink address="https://github.com/KennethTrecy?tab=repositories">
+						list of my respositories
+					</ExternalLink>
+					in
+					<BaseLink
+						address="https://github.com/KennethTrecy"
+						relationship={[ ...externalTypes, ...authorTypes ]}
+						target="other">my GitHub profile</BaseLink>.
+				</p>
 			</div>
-			<p class="mt-4">
-				Other projects can be found on the
-				<ExternalLink address="https://github.com/KennethTrecy?tab=repositories">
-					list of my respositories
-				</ExternalLink>
-				in
-				<BaseLink
-					address="https://github.com/KennethTrecy"
-					relationship={[ ...externalTypes, ...authorTypes ]}
-					target="other">my GitHub profile</BaseLink>.
-			</p>
-		</div>
-	</section>
-	<section
-		itemprop="hasPart"
-		itemscope
-		itemtype="https://schema.org/WebContent"
-		class="container my-4 flex flex-col justify-center items-center">
-		<div class="prose md:prose-lg">
-			<h2 itemprop="headline">Security in mind</h2>
-			<p itemprop="text">I also like reading articles about cybersecurity. For this reason, I build applications with security in mind as much as possible to be resilient from attacks.</p>
-			<p itemprop="text">I have also made my recent commits to be signed with my own cryptographic keys.</p>
-		</div>
-	</section>
+		</section>
+		<section
+			itemprop="hasPart"
+			itemscope
+			itemtype="https://schema.org/WebContent"
+			class="flex flex-col justify-center items-center">
+			<div class="container max-w-prose md:px-0">
+				<h2 itemprop="headline">Security in mind</h2>
+				<p itemprop="text">
+					I also like reading articles about cybersecurity. For this reason, I build applications with security in mind as much as possible to be resilient from attacks.
+				</p>
+				<p itemprop="text">
+					I have also made my recent commits to be signed with my own cryptographic keys.
+				</p>
+			</div>
+		</section>
+	</svelte:fragment>
 </MainArticle>
 
 <style lang="postcss">
