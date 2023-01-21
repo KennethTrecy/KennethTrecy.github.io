5c39f4c304543c36e08d37baa892c377e13c406d
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-21 20:35:42 +0800

    internal(component): allow addition of title and class to base link

--- src/components/general/links/base.svelte
+++ src/components/general/links/base.svelte
@@ -4,11 +4,20 @@
 	export let address: string
 	export let context: AnchorTarget = "self"
 	export let relationship: AnchorLinkType|AnchorLinkType[]
+	export let title: string = ""
+	let group: string[] = []
 
+	export { group as class }
+
+	$: specifiedTitle = title ? title : undefined
 	$: relationshipTypes = Array.isArray(relationship) ? relationship.sort().join(" ") : relationship
 	$: target = context === "self" ? "_self" : "_blank"
 </script>
 
-<a href={address} class="link" rel={relationshipTypes} {target}>
+<a
+	title={specifiedTitle}
+	href={address}
+	class={[ "link", ...group ].join(" ")}
+	rel={relationshipTypes} {target}>
 	<slot></slot>
 </a>
