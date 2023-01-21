553a65ab62e030bde41d8e1acf552bc0b97f67b0
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-21 13:58:46 +0800

    internal(component): make component for external links

---
+++ src/components/general/external_link.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Link from "@/components/general/link.svelte"
+
+	export let address: string
+</script>
+
+<Link address={address} relationship={[ "external", "noopener" ]} context="other">
+	<slot></slot>
+</Link>
