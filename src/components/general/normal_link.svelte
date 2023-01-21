a53edc6534042442dab32f4bc6dff3de9401baa8
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-21 13:15:43 +0800

    internal(component): add anchor links types

--- src/components/general/normal_link.svelte
+++ src/components/general/normal_link.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import { AnchorLinkType } from "@/types/links"
+
 	export let address: string
+	export let types: AnchorLinkType|AnchorLinkType[]
+
+	$: relationship = Array.isArray(types) ? types.join(" ") : types
 </script>
 
-<a href={address} class="link">
+<a href={address} class="link" rel={relationship}>
 	<slot></slot>
 </a>
