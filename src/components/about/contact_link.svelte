886fc84589ad8e3f07bf011ecf35fcec27b36fbb
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-21 16:26:45 +0800

    internal(component): add other possible relationships

--- src/components/about/contact_link.svelte
+++ src/components/about/contact_link.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
+	import Link from "@/components/general/link.svelte"
 	import BrandIcon from "@/components/general/brand_icon.svelte"
 
 	export let address: string
 	export let icon: string
 	export let platformName: string
 	export let username: string
+	export let isExternal: boolean
+
+	$: relationships = [
+		...isExternal ? [ "noopener", "external" ] : [ "opener" ],
+		"author", "me"
+	]
 </script>
 
-<a
-	title={platformName}
-	href={address}
-	rel="me"
-	class="inline-flex flex-row flex-nowrap items-center">
-	<BrandIcon {icon} alternateName={platformName}/>
-	<span class="ml-4">{username}</span>
-</a>
+<Link {address} relationship={relationships}>
+	<span title={platformName} class="inline-flex flex-row flex-nowrap items-center">
+		<BrandIcon {icon} alternateName={platformName}/>
+		<span class="ml-4">{username}</span>
+	</span>
+</Link>
