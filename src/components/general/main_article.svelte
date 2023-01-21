611ce49914f5b70567975915187a9e2f0450f2d7
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-22 02:13:54 +0800

    internal(component): re-layout the main article

--- src/components/general/main_article.svelte
+++ src/components/general/main_article.svelte
@@ -2,18 +2,22 @@
 	export let itemtype: string = "https://schema.org/WebPageContent"
 </script>
 
-<article
-	itemprop="mainContentOfPage"
-	itemscope
-	itemtype="https://schema.org/WebPageElement"
-	class="h-full w-full flex flex-col justify-center items-center">
-	<slot name="title"></slot>
+<div class="h-full w-full flex flex-col justify-center items-center container">
 	<article
-		itemprop="mainEntity"
+		itemprop="mainContentOfPage"
 		itemscope
-		{itemtype}
-		class="pb-8">
-		<slot name="content"></slot>
+		itemtype="https://schema.org/WebPageElement"
+		class="prose md:prose-lg flex flex-col justify-center items-center">
+		<slot name="title"></slot>
+		<div
+			itemprop="mainEntity"
+			itemscope
+			{itemtype}
+			class="pb-8 text-justify">
+			<slot name="content"></slot>
+		</div>
+		<article class="pb-8">
+			<slot name="metadata"></slot>
+		</article>
 	</article>
-	<slot name="metadata"></slot>
-</article>
+</div>
