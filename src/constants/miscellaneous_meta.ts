54d2ad12e9bf7811ca647032252054d048815852
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-22 13:17:24 +0800

    internal(constant): make first publication date

--- src/constants/miscellaneous_meta.ts
+++ src/constants/miscellaneous_meta.ts
@@ -1,1 +1,6 @@
 export const LICENSE = "MIT"
+
+/**
+ * Indicate when the site was first published. Helpful for the initial pages of the site.
+ */
+export const FIRST_PUBLICATION_DATE = new Date(2023, 2, 1)
