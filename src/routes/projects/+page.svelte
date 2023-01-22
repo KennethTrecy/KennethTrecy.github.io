106c75c7b12393d0ee3a047f77bbf55b458c95d2
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-22 12:42:46 +0800

    feat(project): make a page for list of projects

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import CommonHead from "@/components/general/common_head.svelte"
+	import MainArticle from "@/components/general/main_article.svelte"
+</script>
+
+<svelte:head>
+	<CommonHead
+		title="Projects | KennethTrecy"
+		description="List of personal open-source projects made by Kenneth Trecy"
+		keywords={[ "Kenneth Trecy", "personal", "projects" ]}
+		pageVersion="0.1"/>
+</svelte:head>
+
+<MainArticle itemtype="https://schema.org/CreativeWorkSeries">
+	<h1 slot="title">List of Personal Projects</h1>
+	<svelte:fragment slot="content">
+		<section
+			itemprop="about"
+			itemscope
+			itemtype="https://schema.org/WebContent">
+			<h2 itemprop="headline">
+				Introduction
+			</h2>
+			<p itemprop="mainEntity">
+				Over the years, I have built multiple projects to apply my knowledge and gain experience on to build them, to solve their technical issues, and to improve their technical design if possible.
+			</p>
+		</section>
+		<section
+			itemprop="hasPart"
+			itemscope
+			itemtype="https://schema.org/WebContent"
+			class="flex flex-col justify-center items-center">
+
+		</section>
+		<section
+			itemprop="hasPart"
+			itemscope
+			itemtype="https://schema.org/WebContent"
+			class="flex flex-col justify-center items-center">
+
+		</section>
+	</svelte:fragment>
+</MainArticle>
