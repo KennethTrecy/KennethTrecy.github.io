88dd500622b588a4c11da6a4e816cc61d9854f2a
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-22 15:34:07 +0800

    internal(project): structure the projects

--- src/components/general/project_card.svelte
+++ src/components/general/project_card.svelte
@@ -6,13 +6,16 @@
 	export let link: string
 </script>
 
-<div class="card not-prose flex-1 bg-base-200 mr-4 mb-4">
+<div
+	itemprop="hasPart"
+	itemscope itemtype="https://schema.org/CreativeWork"
+	class="card not-prose flex-1 bg-base-200 mr-4 mb-4">
 	<div class="card-body">
-		<a class="card-title flex flex-row items-center" href={link}>
-			<h4>{title}</h4>
+		<a itemprop="mainEntityOfPage" class="card-title flex flex-row items-center" href={link}>
+			<h4 itemprop="name">{title}</h4>
 			<Icon name="chevron_right"/>
 		</a>
-		<p class="prose">
+		<p itemprop="about" class="prose">
 			{description}
 		</p>
 	</div>
