21c0a2d28092991b7715e511ad13c2db768e0bce
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-22 13:57:51 +0800

    internal(constant): treat the first publication date into UTC timezone

--- src/constants/miscellaneous_meta.ts
+++ src/constants/miscellaneous_meta.ts
@@ -3,4 +3,4 @@ export const LICENSE = "MIT"
 /**
  * Indicate when the site was first published. Helpful for the initial pages of the site.
  */
-export const FIRST_PUBLICATION_DATE = new Date(2023, 2, 1)
+export const FIRST_PUBLICATION_DATE = new Date(Date.UTC(2023, 1, 1))
