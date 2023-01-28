b22551c895856d02b4a8de80b885d92c941d8ed9
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-28 22:34:33 +0800

    internal(article): make meta for article index page

---
+++ src/routes/articles/meta.ts
@@ -0,0 +1,13 @@
+import { SECOND_PUBLICATION_DATE } from "@/constants/miscellaneous_meta"
+
+import definePageMeta from "@/components/general/define_page_meta"
+
+const meta = definePageMeta("/", {
+	"datePublished": SECOND_PUBLICATION_DATE,
+	"description": "Collection of articles written by Kenneth Trecy",
+	"keywords": [ "Kenneth Trecy", "articles" , "collection" ],
+	"title": "KennethTrecy's Article Collection",
+	"version": "0.1"
+})
+
+export default meta
