8345d8d27b08a9fe54893a79d54a71a7ce82e05b
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-28 16:20:06 +0800

    internal(about): prepare the meta information by duplication

--- src/routes/about/meta.ts
+++ src/routes/about/meta.ts
@@ -3,7 +3,10 @@ import definePageMeta from "@/components/general/define_page_meta"
 
 const meta = definePageMeta("/about", {
 	"version": "0.1",
-	"datePublished": FIRST_PUBLICATION_DATE
+	"datePublished": FIRST_PUBLICATION_DATE,
+	"title": "Who am I?",
+	"description": "Know more about Kenneth Trecy's journey and his skills on I.T. field. Contact details are also listed here if you wnt to connect with him.",
+	"keywords": [ "Kenneth Trecy", "contact" , "journey" ]
 })
 
 export default meta
