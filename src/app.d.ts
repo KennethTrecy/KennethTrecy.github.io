64e4cf28e51317b94742bb1443569899497bcf38
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-28 14:38:03 +0800

    internal: add necessary environment variables

--- src/app.d.ts
+++ src/app.d.ts
@@ -5,5 +5,14 @@ declare namespace App {
 	// interface Error {}
 	// interface Locals {}
 	// interface PageData {}
-	// interface Platform {}
+	interface Platform {}
+	interface Platform {
+		env: {
+			COUNTER: DurableObjectNamespace;
+		};
+		context: {
+			waitUntil(promise: Promise<any>): void;
+		};
+		caches: CacheStorage & { default: Cache }
+	}
 }
