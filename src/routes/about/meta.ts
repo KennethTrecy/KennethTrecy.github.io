4e5adcca44352001a298fb9f2170e37cb6bb4929
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-28 23:27:26 +0800

    feat(about): improve the grammar of description

--- src/routes/about/meta.ts
+++ src/routes/about/meta.ts
@@ -3,7 +3,7 @@ import definePageMeta from "@/components/general/define_page_meta"
 
 const meta = definePageMeta("/about", {
 	"datePublished": FIRST_PUBLICATION_DATE,
-	"description": "Know more about Kenneth Trecy's journey and his skills on I.T. field. Contact details are also listed here if you wnt to connect with him.",
+	"description": "Know more about Kenneth Trecy's journey and his skills on I.T. field. Contact details have been listed here too if you want to connect with him.",
 	"keywords": [ "Kenneth Trecy", "contact" , "journey" ],
 	"title": "Who am I?",
 	"version": "0.1"
