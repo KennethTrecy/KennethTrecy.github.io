d8da70f2743697fd7ba9a97dbbaffe0aad1c0384
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-28 09:31:19 +0800

    feat: add separate menu item for about self page

--- src/components/shell/menu.svelte
+++ src/components/shell/menu.svelte
@@ -12,8 +12,12 @@
 			<span class="material-symbols-outlined">code</span>
 			<span class="flex-1">Projects<span>
 		</a>
+		<a href="/about" class="inline-flex flex-row flex-nowrap items-center">
+			<span class="material-symbols-outlined">badge</span>
+			<span class="flex-1">About me<span>
+		</a>
 		<a href="/about#contact_details" class="inline-flex flex-row flex-nowrap items-center">
-			<span class="material-symbols-outlined">contact_page</span>
+			<span class="material-symbols-outlined">contacts</span>
 			<span class="flex-1">Contacts<span>
 		</a>
 	</li>
