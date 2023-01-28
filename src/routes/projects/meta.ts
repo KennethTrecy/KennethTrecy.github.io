a20ea6de2e76c2934e45f86e04813f5e0104dcc5
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-28 16:21:31 +0800

    internal(project): prepare the meta information by duplication

--- src/routes/projects/meta.ts
+++ src/routes/projects/meta.ts
@@ -3,7 +3,10 @@ import definePageMeta from "@/components/general/define_page_meta"
 
 const meta = definePageMeta("/projects", {
 	"version": "0.1",
-	"datePublished": FIRST_PUBLICATION_DATE
+	"datePublished": FIRST_PUBLICATION_DATE,
+	title: "Projects",
+	description: "Kenneth Trecy is has a decade of experience in programming websites. In this page, an overview of different projects he was involve have been listed.",
+	keywords: [ "Kenneth Trecy", "involved", "personal", "projects" ]
 })
 
 export default meta
