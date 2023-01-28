4366fafbdd39bc4c93376e496920fbb324cbbc82
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-28 22:38:16 +0800

    internal(constant): specify the second publishing date

--- src/constants/miscellaneous_meta.ts
+++ src/constants/miscellaneous_meta.ts
@@ -4,3 +4,8 @@ export const LICENSE = "MIT"
  * Indicate when the site was first published. Helpful for the initial pages of the site.
  */
 export const FIRST_PUBLICATION_DATE = new Date(Date.UTC(2023, 1, 1))
+
+/**
+ * Indicate when the articles was first published.
+ */
+export const SECOND_PUBLICATION_DATE = new Date(Date.UTC(2023, 1, 15))
