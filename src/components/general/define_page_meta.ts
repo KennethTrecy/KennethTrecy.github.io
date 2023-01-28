b565e1c7ea1db7c3b2921a951dc30251001a1932
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-28 15:58:26 +0800

    internal(component): make function to define page meta

---
+++ src/components/general/define_page_meta.ts
@@ -0,0 +1,33 @@
+import type { PageMeta } from "@/types/head"
+
+import { WEBSITE_OWNER } from "@/constants/names"
+import { LICENSE } from "@/constants/miscellaneous_meta"
+
+export default function(path: string, {
+	version = "0.1",
+	datePublished = new Date(),
+	dateModified = datePublished,
+	title = "Draft Page",
+	description = "This is a draft page.",
+	keywords = [ "draft" ],
+	author = WEBSITE_OWNER,
+	encoder = author,
+	designer = [ encoder ],
+	creator = encoder,
+	license = LICENSE
+}: Partial<Exclude<PageMeta, "path">>): PageMeta {
+	return {
+		path,
+		version,
+		datePublished,
+		dateModified,
+		title,
+		description,
+		keywords,
+		author,
+		encoder,
+		designer,
+		creator,
+		license
+	}
+}
