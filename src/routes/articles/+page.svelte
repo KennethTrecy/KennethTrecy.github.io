047843fe9e97dde406cc8abe3d1b0ea8ba04bfbc
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-28 22:37:35 +0800

    feat(article): make article index page

---
+++ src/routes/articles/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import pageMeta from "@/routes/articles/meta"
+
+	import Heading from "@/components/general/heading.svelte"
+	import CommonHead from "@/components/general/common_head.svelte"
+	import MainArticle from "@/components/general/main_article.svelte"
+	import PageDetailCard from "@/components/general/independent_page_detail_card.svelte"
+</script>
+
+<svelte:head>
+	<CommonHead {pageMeta}/>
+</svelte:head>
+
+<MainArticle>
+	<Heading slot="title" class="text-center md:text-left">{pageMeta.title}</Heading>
+	<svelte:fragment slot="content">
+		<div
+			itemprop="mainEntity"
+			itemscope
+			itemtype=""
+			class="">
+			<section class="">
+
+			</section>
+		</div>
+	</svelte:fragment>
+	<PageDetailCard slot="metadata" {pageMeta}/>
+</MainArticle>
