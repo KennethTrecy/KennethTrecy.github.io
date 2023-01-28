6b73ecd4376efe1bc11949490dfb0f0f455fcb58
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-28 15:57:34 +0800

    internal(type): remove access specifier

--- src/types/head.ts
+++ src/types/head.ts
@@ -2,60 +2,60 @@ export interface PageMeta {
 	/**
 	 * Path of the document within the website.
 	 */
-	 public readonly path: string
+	readonly path: string
 
 	 /**
 	  * Version of the document.
 	  */
-	 public readonly version: string
+	readonly version: string
 
 	 /**
 	  * First published date of the document.
 	  */
-	 public readonly datePublished: Date
+	readonly datePublished: Date
 
 	 /**
 	  * Modification date of the document.
 	  */
-	 public readonly dateModified: Date
+	readonly dateModified: Date
 
 	 /**
 	  * title of the document
 	  */
-	 public readonly title: string
+	readonly title: string
 
 	 /**
 	  * Description of the document.
 	  */
-	 public readonly description: string
+	readonly description: string
 
 	 /**
 	  * Keywords to understand the document.
 	  */
-	 public readonly keywords: string[]
+	readonly keywords: string[]
 
 	 /**
 	  * Author of the document (wrote the contents in either pen-and-paper or virtually).
 	  */
-	 public readonly author: string
+	readonly author: string
 
 	 /**
 	  * The one who typed the document into web page.
 	  */
-	 public readonly encoder: string
+	readonly encoder: string
 
 	 /**
 	  * the one who designed the document.
 	  */
-	 public readonly designer: string[]
+	readonly designer: string[]
 
 	 /**
 	  * Person who instructed to compile the document into web page.
 	  */
-	 public readonly creator: string
+	readonly creator: string
 
 	 /**
 	  * License of the document.
 	  */
-	 public readonly license: string
+	readonly license: string
 }
