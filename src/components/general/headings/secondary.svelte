eba58ef8c9103a5cfe9db891cf0beb6fd77551d0
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-29 17:45:55 +0800

    chore: prepare secondary header by duplication

---
+++ src/components/general/headings/secondary.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	let otherClasses: string = ""
+	export let prefix: string = ""
+
+	export { otherClasses as class }
+
+	$: joinedClasses = [ "prose md:prose-lg", otherClasses ].filter(Boolean).join(" ")
+	$: hasPrefix = prefix !== ""
+</script>
+
+<h1 class={joinedClasses}>
+	{#if hasPrefix}
+		<span class="text-5xl">
+			<span>{prefix}</span>
+			<span itemprop="headline name"><slot></slot></span>
+		</span>
+	{:else}
+		<span itemprop="headline name" class="text-5xl">
+			<slot></slot>
+		</span>
+	{/if}
+</h1>
