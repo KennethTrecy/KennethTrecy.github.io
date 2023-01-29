f9c69544e83fc5e4ae5cf7cc94813d8d74a92c08
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-29 22:41:26 +0800

    internal(component): allow bookmark to be a property

--- src/components/general/links/bookmark.svelte
+++ src/components/general/links/bookmark.svelte
@@ -4,8 +4,9 @@
 	import Link from "@/components/general/links/base.svelte"
 
 	export let fragment: string
+	export let itemprop: string|undefined = undefined
 </script>
 
-<Link address={fragment} relationship={[ "bookmark", ...internalTypes ]} context="self">
+<Link address={fragment} relationship={[ "bookmark", ...internalTypes ]} context="self" {itemprop}>
 	<slot></slot>
 </Link>
