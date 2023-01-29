20eb35ec0385601d115e131f68e1bfb6bf8c84eb
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-29 22:13:31 +0800

    internal(component): make a bookmark component

---
+++ src/components/general/links/bookmark.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { internalTypes } from "@/components/general/links/constants"
+
+	import Link from "@/components/general/links/base.svelte"
+
+	export let fragment: string
+</script>
+
+<Link address={fragment} relationship={[ "bookmark", ...internalTypes ]} context="self">
+	<slot></slot>
+</Link>
