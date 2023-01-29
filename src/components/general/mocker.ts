5985e39c27b889bf1b0a7ee5364b9d0d8c3a3160
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-29 18:43:48 +0800

    internal(component): make singleton to mock browser features in tests

---
+++ src/components/general/mocker.ts
@@ -0,0 +1,22 @@
+import { browser } from "$app/environment"
+import type { MockInfo } from "@/constants/test_interface"
+
+export default class {
+	private static infos: MockInfo[] = []
+
+	static mock(mockLogic: () => MockInfo, realLogic: () => void): void {
+		if (browser) {
+			realLogic()
+		} else {
+			this.infos.push(mockLogic())
+		}
+	}
+
+	static unmockAll(): MockInfo[] {
+		const infos = this.infos
+
+		this.infos = []
+
+		return infos
+	}
+}
