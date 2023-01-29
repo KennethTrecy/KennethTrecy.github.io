af6447ee7f19bfa65578c450bb1228a28a94d4bf
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-29 21:35:24 +0800

    internal(constant): add meta information of website info in collection

--- src/constants/meta_collection.ts
+++ src/constants/meta_collection.ts
@@ -3,11 +3,13 @@ import type { PageMeta } from "@/types/head"
 import indexMeta from "@/routes/meta"
 import aboutMeta from "@/routes/about_myself/meta"
 import projectsMeta from "@/routes/projects/meta"
+import websiteInfoMeta from "@/routes/website_info/meta"
 
 const metaCollection: PageMeta[] = [
 	indexMeta,
 	aboutMeta,
-	projectsMeta
+	projectsMeta,
+	websiteInfoMeta
 ]
 
 export default metaCollection
