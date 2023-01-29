d06504fba71a063b1d957480558f9e97b54c8043
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-29 18:00:22 +0800

    test(unit): prepare test for secondary header

--- src/components/general/headings/secondary.spec.ts
+++ src/components/general/headings/secondary.spec.ts
@@ -1,14 +1,15 @@
 // @vitest-environment jsdom
 
 import { describe, it, expect } from "vitest"
-import { render, cleanup } from "@testing-library/svelte"
+import { render, cleanup, fireEvent } from "@testing-library/svelte"
 
 import Component from "./primary.svelte"
 
-describe("Primary header behavior", () => {
+describe("Secondary header behavior", () => {
 	it("can render prefix as separate", async () => {
 		const prefix = "I. "
-		const { container } = render(Component, { prefix })
+		const id = "hello_a"
+		const { container } = render(Component, { prefix, id })
 
 		const property = container.querySelector("[itemprop~=headline][itemprop~=name]")
 
@@ -18,12 +19,40 @@ describe("Primary header behavior", () => {
 		cleanup()
 	})
 
-	it("can render one span if there is no prefix", async () => {
-		const { container } = render(Component)
+	it("can render two spans if there is no prefix", async () => {
+		const id = "hello_b"
+		const { container } = render(Component, { id })
 
-		const spans = container.querySelectorAll("h1 > span")
+		const spans = container.querySelectorAll("h2 > span")
 
-		expect(spans).toHaveLength(1)
+		expect(spans).toHaveLength(2)
+
+		cleanup()
+	})
+
+	it("can show link copier if hovered", async () => {
+		const id = "hello_c"
+		const { container } = render(Component, { id })
+
+		const header = container.querySelector("h2") as HTMLHeadingElement
+		await fireEvent.mouseOver(header)
+
+		const linkCopier = container.querySelector("span.visible")
+		expect(linkCopier).not.toBeNull()
+
+		cleanup()
+	})
+
+	it("can hide link copier if unhovered", async () => {
+		const id = "hello_d"
+		const { container } = render(Component, { id })
+
+		const header = container.querySelector("h2") as HTMLHeadingElement
+		await fireEvent.mouseOver(header)
+		await fireEvent.mouseOut(header)
+
+		const linkCopier = container.querySelector("span.invisible")
+		expect(linkCopier).not.toBeNull()
 
 		cleanup()
 	})
