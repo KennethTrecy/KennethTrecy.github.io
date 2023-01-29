5459997a46b33dcde1f28efaed34414d6b31c373
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-29 16:29:01 +0800

    internal(component): render different elements based on prefix

--- src/components/general/heading/primary.svelte
+++ src/components/general/heading/primary.svelte
@@ -1,13 +1,26 @@
 <script lang="ts">
 	let otherClasses: string = ""
+	export let prefix: string = ""
 
 	export { otherClasses as class }
+
+	$: joinedClasses = [ "prose md:prose-lg", otherClasses ].filter(Boolean).join(" ")
+	$: hasPrefix = prefix !== ""
 </script>
 
-<h1
-	class={[ "prose md:prose-lg", otherClasses ].filter(Boolean).join(" ")}
-	itemprop="headline name">
-	<span class="text-5xl">
-		<slot></slot>
-	</span>
-</h1>
+{#if hasPrefix}
+	<h1 class={joinedClasses}>
+		<span class="text-5xl">
+			<span>{prefix}</span>
+			<span itemprop="headline name"><slot></slot></span>
+		</span>
+	</h1>
+{:else}
+	<h1
+		class={joinedClasses}
+		itemprop="headline name">
+		<span class="text-5xl">
+			<slot></slot>
+		</span>
+	</h1>
+{/if}
