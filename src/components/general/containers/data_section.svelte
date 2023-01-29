7be7294c5391f786e24bb8b01ab643df15582ba0
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-30 01:24:37 +0800

    internal(component): make a general data section

---
+++ src/components/general/containers/data_section.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let id: string
+	export let itemprop: string = "hasPart"
+	export let itemtype: string = "https://schema.org/WebContent"
+
+	let otherClasses: string[] = []
+
+	export { otherClasses as class }
+</script>
+
+<section {itemprop} itemscope {itemtype} class={otherClasses.join(" ")} {id}>
+	<slot></slot>
+</section>
