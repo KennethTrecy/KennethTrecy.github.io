e4bf3fa07ad14952850793de6fe35d8e58d0f45b
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-29 11:50:44 +0800

    perf(shell): pre-connect to optimize loading of font resources

--- src/components/shell.svelte
+++ src/components/shell.svelte
@@ -13,6 +13,8 @@
 </script>
 
 <svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link
 		rel="stylesheet"
 		href="https://fonts.googleapis.com/css2?family=Roboto&family=Material+Symbols+Outlined:wght,FILL@400,1&display=swap"/>
