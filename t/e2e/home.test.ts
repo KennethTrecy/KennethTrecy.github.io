057111e6b5e82c9eff10237155ad62d2ccb84231
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-01 22:04:37 +0800

    test(e2e): add other selectors containing texts to validate

--- t/e2e/home.test.ts
+++ t/e2e/home.test.ts
@@ -4,7 +4,7 @@ import { expect, test } from "@playwright/test"
 test("paragraphs have correct grammar", async ({ page }) => {
 	await page.goto("/")
 
-	const allTexts = await page.locator("css=p[itemprop]").allInnerTexts()
+	const allTexts = await page.locator("css=p[itemprop], h1, h2, a span.flex-1").allInnerTexts()
 
 	const pendingResults: Promise<any>[] = allTexts.map(async text => {
 		const result = await check(text, {
