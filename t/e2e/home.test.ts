e4182b27162ad1e6962dff5950e8fe6fa483f1bb
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-01 23:19:40 +0800

    test(e2e): generalize the checking of grammar

--- t/e2e/home.test.ts
+++ t/e2e/home.test.ts
@@ -1,22 +1,10 @@
 import { check } from "gramma"
-import { expect, test } from "@playwright/test"
+import { test } from "@playwright/test"
 
-import dictionary from "~/data/dictionary"
+import checkGrammar from "~/utilities/check_grammar"
 
-test("paragraphs have correct grammar", async ({ page }) => {
+test("page has correct grammar", async ({ page }) => {
 	await page.goto("/")
 
-	const allTexts = await page.locator("css=.menu a span.flex-1, [itemprop~=text], h1, h2").allInnerTexts()
-
-	const pendingResults: Promise<any>[] = allTexts.map(async text => {
-		const result = await check(text, {
-			"api_url": "http://localhost:8081/v2/check",
-			dictionary
-		})
-
-		return result.matches
-	})
-
-	const matches = await Promise.all(pendingResults)
-	await expect(matches).toEqual(allTexts.map(() => []))
+	await checkGrammar(page)
 })
