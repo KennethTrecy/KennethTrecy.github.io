3ec485fb01b67205faaf83853e47e1732ebae727
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-04 23:09:56 +0800

    build: remove alias paths for tests and traverse manually

--- t/e2e/home.test.ts
+++ t/e2e/home.test.ts
@@ -1,7 +1,7 @@
 import { check } from "gramma"
 import { test } from "@playwright/test"
 
-import checkGrammar from "~/utilities/check_grammar"
+import checkGrammar from "../utilities/check_grammar"
 
 test("page has correct grammar", async ({ page }) => {
 	await page.goto("/")
