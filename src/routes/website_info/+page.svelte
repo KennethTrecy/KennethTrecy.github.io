b72ae5eb9f83c1fa58ecb79b04662e84e17267b2
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-04 20:33:21 +0800

    feat(website info): improve the section with tertiary headings

--- src/routes/website_info/+page.svelte
+++ src/routes/website_info/+page.svelte
@@ -5,8 +5,9 @@
 	import BaseLink from "@/components/general/links/base.svelte"
 	import CommonHead from "@/components/general/common_head.svelte"
 	import ExternalLink from "@/components/general/links/external.svelte"
-	import PrimaryHeader from "@/components/general/headings/primary.svelte"
 	import defineHeadingInfo from "@/components/general/define_heading_info"
+	import PrimaryHeading from "@/components/general/headings/primary.svelte"
+	import TertiaryHeading from "@/components/general/headings/tertiary.svelte"
 	import SimpleText from "@/components/general/containers/simple_text.svelte"
 	import SecondaryHeading from "@/components/general/headings/secondary.svelte"
 	import ThirdPartyLink from "@/components/shell/third-party_package_link.svelte"
@@ -29,6 +30,10 @@
 	const motivation = defineHeadingInfo({
 		"prefix": "🚴🏽",
 		"text": "The Motivation"
+	})
+	const dependencies = defineHeadingInfo({
+		"prefix": "📦",
+		"text": "Dependencies"
 	})
 	const bundledDependencies = defineHeadingInfo({
 		"prefix": "🧬",
@@ -105,7 +110,7 @@
 </svelte:head>
 
 <StructuredArticle itemtype="https://schema.org/Website">
-	<PrimaryHeader slot="title">{pageMeta.title}</PrimaryHeader>
+	<PrimaryHeading slot="title">{pageMeta.title}</PrimaryHeading>
 	<svelte:fragment slot="content">
 		<StructuredSection itemtype="https://schema.org/WebContent" id={motivation.id}>
 			<SecondaryHeading headingInfo={motivation}/>
@@ -113,54 +118,60 @@
 				The purpose of this website is to showcase my projects that I have built. The website itself is also project where I can apply my different skills. It also contains valuable information to connect me and my future employers.
 			</SimpleText>
 		</StructuredSection>
-		<StructuredSection itemtype="https://schema.org/ItemList" id={bundledDependencies.id}>
-			<SecondaryHeading headingInfo={bundledDependencies}/>
+		<StructuredSection id={dependencies.id}>
+			<SecondaryHeading headingInfo={dependencies}/>
 			<SimpleText itemprop="about">
-				The website uses several open-source projects. Some dependencies (which are included during bundling) have been mentioned below.
+				The website uses several open-source projects and third-party services to build and host it. They may be linked in the final output. Some are included in the bundling process. Some make up the deployment process.
 			</SimpleText>
-			<StructuredList order="unordered" isProjectList={false} hasOwnScope={false}>
-				{#each compiledThirdPartyPackages as thirdPartyPackage}
-					<StructuredListItem itemtype="https://schema.org/SoftwareApplication">
-						<ThirdPartyLink
-							packageName={thirdPartyPackage.name}
-							homeLink={thirdPartyPackage.homepage}
-							licenseName={thirdPartyPackage.licenseName}
-							licenseLink={thirdPartyPackage.licenseLink}/>
-					</StructuredListItem>
-				{/each}
-			</StructuredList>
-		</StructuredSection>
-		<StructuredSection itemtype="https://schema.org/ItemList" id={linkedDependencies.id}>
-			<SecondaryHeading headingInfo={linkedDependencies}/>
-			<SimpleText itemprop="about">
-				Meanwhile, some dependencies have been used in the website by using <code>&lt;link&gt;</code> tags or through <code>src</code> attribute of <code>&lt;img&gt;</code> tags. They are included in the rendering process of a user's browser to enhance appearance.
-			</SimpleText>
-			<StructuredList order="unordered" isProjectList={false} hasOwnScope={false}>
-				{#each linkedThirdPartyPackages as thirdPartyPackage}
-					<StructuredListItem itemtype="https://schema.org/SoftwareApplication">
-						<ThirdPartyLink
-							packageName={thirdPartyPackage.name}
-							homeLink={thirdPartyPackage.homepage}
-							licenseName={thirdPartyPackage.licenseName}
-							licenseLink={thirdPartyPackage.licenseLink}/>
-					</StructuredListItem>
-				{/each}
-			</StructuredList>
-		</StructuredSection>
-		<StructuredSection id={deployment.id}>
-			<SecondaryHeading headingInfo={deployment}/>
-			<SimpleText itemprop="mainEntity">
-				To build the website, <ThirdPartyLink
-					packageName="SvelteKit"
-					homeLink="https://kit.svelte.dev/"
-					licenseName="MIT"
-					licenseLink="https://github.com/sveltejs/kit/blob/master/LICENSE"/>
-				has been used to bundle necessary resources for each web page.
-				The bundling happens in
-				<ExternalLink address="https://developers.cloudflare.com/pages/">
-					Cloudflare Pages
-				</ExternalLink> then, the bundled resources will be deployed globally.
-			</SimpleText>
+			<StructuredSection itemtype="https://schema.org/ItemList" id={bundledDependencies.id}>
+				<TertiaryHeading headingInfo={bundledDependencies}/>
+				<SimpleText itemprop="about">
+					 Some dependencies which are included during bundling have been mentioned below. These dependencies have significant impact on source code of this website.
+				</SimpleText>
+				<StructuredList order="unordered" isProjectList={false} hasOwnScope={false}>
+					{#each compiledThirdPartyPackages as thirdPartyPackage}
+						<StructuredListItem itemtype="https://schema.org/SoftwareApplication">
+							<ThirdPartyLink
+								packageName={thirdPartyPackage.name}
+								homeLink={thirdPartyPackage.homepage}
+								licenseName={thirdPartyPackage.licenseName}
+								licenseLink={thirdPartyPackage.licenseLink}/>
+						</StructuredListItem>
+					{/each}
+				</StructuredList>
+			</StructuredSection>
+			<StructuredSection itemtype="https://schema.org/ItemList" id={linkedDependencies.id}>
+				<TertiaryHeading headingInfo={linkedDependencies}/>
+				<SimpleText itemprop="about">
+					Meanwhile, some dependencies have been used in the website by using <code>&lt;link&gt;</code> tags or through <code>src</code> attribute of <code>&lt;img&gt;</code> tags. They are included in the rendering process of a user's browser to enhance appearance.
+				</SimpleText>
+				<StructuredList order="unordered" isProjectList={false} hasOwnScope={false}>
+					{#each linkedThirdPartyPackages as thirdPartyPackage}
+						<StructuredListItem itemtype="https://schema.org/SoftwareApplication">
+							<ThirdPartyLink
+								packageName={thirdPartyPackage.name}
+								homeLink={thirdPartyPackage.homepage}
+								licenseName={thirdPartyPackage.licenseName}
+								licenseLink={thirdPartyPackage.licenseLink}/>
+						</StructuredListItem>
+					{/each}
+				</StructuredList>
+			</StructuredSection>
+			<StructuredSection id={deployment.id}>
+				<TertiaryHeading headingInfo={deployment}/>
+				<SimpleText itemprop="mainEntity">
+					To build the website, <ThirdPartyLink
+						packageName="SvelteKit"
+						homeLink="https://kit.svelte.dev/"
+						licenseName="MIT"
+						licenseLink="https://github.com/sveltejs/kit/blob/master/LICENSE"/>
+					has been used to bundle necessary resources for each web page.
+					The bundling happens in a JAMstack platform named
+					<ExternalLink address="https://pages.cloudflare.com/">
+						Cloudflare Pages
+					</ExternalLink> then, the bundled resources will be deployed globally.
+				</SimpleText>
+			</StructuredSection>
 		</StructuredSection>
 		<StructuredSection id={concerns.id}>
 			<SecondaryHeading headingInfo={concerns}/>
