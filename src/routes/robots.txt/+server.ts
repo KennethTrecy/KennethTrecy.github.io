7fe610f03407fed3b316002c76bd7b3e4ad3a08d
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-04 21:18:59 +0800

    internal: change the name of environment variable

--- src/routes/robots.txt/+server.ts
+++ src/routes/robots.txt/+server.ts
@@ -1,9 +1,10 @@
-import { CF_PAGES_URL, PRODUCTION_BASE_URL } from "$env/static/private"
+import { CF_PAGES_URL } from "$env/static/private"
+import { PUBLIC_PRODUCTION_BASE_URL } from "$env/static/public"
 
 import metaCollection from "@/constants/meta_collection"
 
 export async function GET() {
-	const documentPaths = CF_PAGES_URL === PRODUCTION_BASE_URL
+	const documentPaths = CF_PAGES_URL === PUBLIC_PRODUCTION_BASE_URL
 		? metaCollection.map(meta => meta.path)
 		: []
 
