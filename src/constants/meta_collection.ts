3fc422e2111194dea6d04b198bac6b44f5c8b4ae
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-07 13:17:59 +0800

    internal(constant): include article meta collection

--- src/constants/meta_collection.ts
+++ src/constants/meta_collection.ts
@@ -2,6 +2,7 @@ import type { PageMeta } from "@/types/head"
 
 import aboutMeta from "@/routes/about_myself/meta"
 import articlesMeta from "@/routes/articles/meta"
+import articleMetaCollection from "@/constants/article_meta_collection"
 import indexMeta from "@/routes/meta"
 import projectsMeta from "@/routes/projects/meta"
 
@@ -9,7 +10,8 @@ const metaCollection: PageMeta[] = [
 	indexMeta,
 	aboutMeta,
 	projectsMeta,
-	articlesMeta
+	articlesMeta,
+	...articleMetaCollection
 ]
 
 export default metaCollection
