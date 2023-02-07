830ac87d5dd0f9b54f3d218f889bdd7392035200
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-07 15:00:30 +0800

    internal(component): prepare the default values of code file

--- src/components/general/containers/example_code.svelte
+++ src/components/general/containers/example_code.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
-	export let code: string
+	import type { CodeFile, CompleteViewableFileInfo } from "@/types/body"
 
-	$: lineOfCode = code.split("\n")
+	export let fileInfo: CompleteViewableFileInfo
+
+	let code: CodeFile = {
+		"content": "",
+		"sha": "",
+		"size": 0,
+		"viewURL": ""
+	}
+	$: codeLines = code.content.split("\n")
 </script>
 
 <div class="mockup-code not-prose">
-	{#each lineOfCode as line, i}
+	{#each codeLines as line, i}
 		<pre data-prefix={i+1}><code>{line}</code></pre>
 	{/each}
 </div>
