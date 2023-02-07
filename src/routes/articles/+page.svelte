392e6d371a717f5ca7e87b877fe8224557f95795
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-07 08:51:59 +0800

    fix(article): correct the path to primary heading

--- src/routes/articles/+page.svelte
+++ src/routes/articles/+page.svelte
@@ -2,7 +2,7 @@
 	import pageMeta from "@/routes/articles/meta"
 
 	import CommonHead from "@/components/general/common_head.svelte"
-	import PrimaryHeading from "@/components/general/heading/primary.svelte"
+	import PrimaryHeading from "@/components/general/headings/primary.svelte"
 	import PageDetailCard from "@/components/general/independent_page_detail_card.svelte"
 	import StructuredArticle from "@/components/general/containers/structured_article.svelte"
 </script>
