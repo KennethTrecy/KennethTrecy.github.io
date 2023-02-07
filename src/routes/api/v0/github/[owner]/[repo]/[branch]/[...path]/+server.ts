4c7c4a1f0d6c6d01a52a2a63ee4375bc80e38528
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-07 09:12:36 +0800

    feat(API): make API to request for code

---
+++ src/routes/api/v0/github/[owner]/[repo]/[branch]/[...path]/+server.ts
@@ -0,0 +1,19 @@
+import type { RequestEvent } from "@sveltejs/kit"
+
+import { Octokit } from "@octokit/core"
+
+import { PERSONAL_GITHUB_ACCESS_TOKEN } from "$env/static/private"
+
+export async function GET(event: RequestEvent) {
+	const octokit = new Octokit({ "auth": PERSONAL_GITHUB_ACCESS_TOKEN })
+
+	const info = await octokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
+		"owner": event.params.owner as string,
+		"repo": event.params.repo as string,
+		"path": event.params.path as string
+	})
+
+	const { html_url, sha, size, content } = info.data as any
+
+	return new Response(JSON.stringify({ html_url, sha, size, content }))
+}
