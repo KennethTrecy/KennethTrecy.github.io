f2bcd037d2bbe0af427d110769041d375107ee22
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-07 15:36:53 +0800

    feat(component): show the metadata of code

--- src/components/general/containers/example_code.svelte
+++ src/components/general/containers/example_code.svelte
@@ -3,6 +3,8 @@
 
 	import type { CodeFile, CompleteViewableFileInfo } from "@/types/body"
 
+	import ExternalLink from "@/components/general/links/external.svelte"
+
 	export let fileInfo: CompleteViewableFileInfo
 
 	let codeInfo: CodeFile = {
@@ -12,6 +14,7 @@
 		"viewURL": ""
 	}
 	$: codeLines = codeInfo.content.split("\n")
+	$: repoURL = `https://github.com/${fileInfo.owner}/${fileInfo.repo}`
 
 	onMount(async () => {
 		const repoNamespace = `${fileInfo.owner}/${fileInfo.repo}`
@@ -26,8 +29,20 @@
 	})
 </script>
 
-<div class="mockup-code not-prose">
-	{#each codeLines as line, i}
-		<pre data-prefix={i+1}><code>{line}</code></pre>
-	{/each}
-</div>
+<section itemprop="workExample" itemscope itemtype="https://schema.org/SoftwareApplication>
+	<div class="mockup-code not-prose text-sm">
+		{#each codeLines as line, i}
+			<pre data-prefix={i+1}><code>{line}</code></pre>
+		{/each}
+	</div>
+	<p itemprop="text" class="text-center text-sm">
+		Content of
+		<ExternalLink
+			address={codeInfo.viewURL}
+			itemprop={[ "mainEntityOfPage" ]}>{fileInfo.path}</ExternalLink> in
+		{fileInfo.branch}
+		at <ExternalLink address={repoURL}>{fileInfo.owner}/{fileInfo.repo}</ExternalLink>.
+		Its size is <span itemprop="fileSize">{codeInfo.size}B</span> in commit
+		<span itemprop="softwareVersion">{codeInfo.sha}</span>.
+	</p>
+</section>
