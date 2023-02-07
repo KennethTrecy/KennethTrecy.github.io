c1cb21d97771d22dccc51a397ad0c83fc784f95a
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-07 14:15:22 +0800

    internal(article): export a list of associated files

--- src/routes/articles/different_levels_of_abstraction_in_software/shared_constants.ts
+++ src/routes/articles/different_levels_of_abstraction_in_software/shared_constants.ts
@@ -1,21 +1,25 @@
 import type { ViewableOwnerInfoTree } from "@/types/body"
 
-export const associatedFileTree: ViewableOwnerInfoTree[] = [
+import flattenOwnerTreeInfo from "@/components/general/flatten_owner_tree_info"
+
+const associatedFileTree: ViewableOwnerInfoTree[] = [
 	{
 		"owner": "KennethTrecy",
 		"repos": [
 			{
 				"repo": "demo_of_different_levels_of_abstraction",
 				"paths": [
+					"src/package-level_abstraction.ts",
 					"src/environment-level_abstraction.ts",
+					"src/variable-level_abstraction.ts",
 					"src/function-level_abstraction_variant_1.ts",
 					"src/function-level_abstraction_variant_2.ts",
 					"src/function-level_abstraction_variant_3.ts",
-					"src/object-level_abstraction.ts",
-					"src/package-level_abstraction.ts",
-					"src/variable-level_abstraction.ts"
+					"src/object-level_abstraction.ts"
 				]
 			}
 		]
 	}
 ]
+
+export const associatedFileList = flattenOwnerTreeInfo(associatedFileTree)
