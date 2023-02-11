6a058a9c4dbbc5dfa9ceb1d5b6b16cb6057b75cb
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-11 23:35:16 +0800

    feat: lower the version number mathematically

--- src/routes/about_myself/meta.ts
+++ src/routes/about_myself/meta.ts
@@ -7,7 +7,7 @@ const meta = definePageMeta("/about_myself", {
 	"description": "Know more about Kenneth Trecy's journey and his skills on I.T. field. Contact details have been listed here too if you want to connect with him.",
 	"keywords": [ "Kenneth Trecy", "contact" , "journey" ],
 	"title": "Who am I?",
-	"version": "1.1-dev"
+	"version": "1.001-dev"
 })
 
 export default meta
--- src/routes/projects/meta.ts
+++ src/routes/projects/meta.ts
@@ -7,7 +7,7 @@ const meta = definePageMeta("/projects", {
 	"description": "Kenneth Trecy has a decade of experience in programming websites. Here is an overview of different projects he was involved.",
 	"keywords": [ "Kenneth Trecy", "involved", "personal", "projects" ],
 	"title": "List of Involved Projects",
-	"version": "1.1-dev"
+	"version": "1.001-dev"
 })
 
 export default meta
