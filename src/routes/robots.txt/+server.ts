2a76a5e673caf90b0b068ef3d99d0acbb261e6b0
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-11 13:30:44 +0800

    fix: correct the variables to determine if the site can be indexed

--- src/routes/robots.txt/+server.ts
+++ src/routes/robots.txt/+server.ts
@@ -1,17 +1,17 @@
-import { CF_PAGES_URL } from "$env/static/private"
+import { IS_INDEXABLE } from "$env/static/private"
 import { PUBLIC_PRODUCTION_BASE_URL } from "$env/static/public"
 
 import metaCollection from "@/constants/meta_collection"
 
 export async function GET() {
-	const documentPaths = CF_PAGES_URL === PUBLIC_PRODUCTION_BASE_URL
+	const documentPaths = IS_INDEXABLE === "true"
 		? metaCollection.map(meta => meta.path)
 		: []
 
 	const allowRules = documentPaths.map(path => `Allow: ${path}$`)
 	const compiledAllowRules = allowRules.join("\n")
 
-	const sitemapRules = [ `Sitemap: ${CF_PAGES_URL}/sitemap.xml` ]
+	const sitemapRules = [ `Sitemap: ${PUBLIC_PRODUCTION_BASE_URL}/sitemap.xml` ]
 	const compiledSitemapRules = sitemapRules.join("\n")
 
 	const userAgentRules = [
