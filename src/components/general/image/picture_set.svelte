dcef50c0181533efa829563539ca2d3b7d56bb25
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-12 12:52:39 +0800

    internal(component): make component to show responsive image

---
+++ src/components/general/image/picture_set.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { MultimediaInfo } from "@/types/content"
+
+	import makeSet from "@/components/general/make_set"
+
+	export let info: MultimediaInfo
+	$: sourceSet = makeSet(info.responsiveLinks)
+</script>
+
+<picture>
+	<source srcset={sourceSet}/>
+	<img src={info.defaultLink} alt={info.description}/>
+</picture>
