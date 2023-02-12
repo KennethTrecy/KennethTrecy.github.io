e24b7d95443518200da49f4afeb76de7592b4218
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-12 12:44:18 +0800

    internal(component): return concatenated source set

--- src/components/general/image/make_set.ts
+++ src/components/general/image/make_set.ts
@@ -1,5 +1,9 @@
 import type { MultimediaLink } from "@/types/content"
 
 export default function(links: MultimediaLink[]): string {
+	if (links.length > 1) {
+		return links.map(linkInfo => `${linkInfo.link} ${linkInfo.intrinsicWidth}w`).join(", ")
+	}
+
 	return ""
 }
