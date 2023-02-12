ae1f1f85dde8f1e4ff611db44d9c6efca7663c32
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-12 22:40:40 +0800

    chore(revert): restore the extraction of texts
    This reverts commit bee866d1803244bc50e7d4518cab0ec153d93224.

--- t/utilities/check_grammar.ts
+++ t/utilities/check_grammar.ts
@@ -3,30 +3,7 @@ import { expect, Page } from "@playwright/test"
 
 import dictionary from "../data/dictionary.ts"
 
-function checkEachGrammar(texts: string[]): Promise<any>[] {
-	return texts.filter(text => text !== "").map(async text => {
-		const result = await check(text, {
-			"api_url": "http://localhost:8081/v2/check",
-			dictionary
-		})
-
-		return result.matches
-	})
-}
-
 export default async function(page: Page) {
-	const metaSelectors = [
-		"description",
-		"keywords"
-	]
-	const allMetaSelectors = metaSelectors.map(name => `meta[name=${name}]`)
-	const allMetaTextGroups = allMetaSelectors.map(
-		selector => ({
-			selector,
-			"locator": page.locator(selector).all()
-		})
-	)
-
 	const innerTextSelectors = [
 		"title",
 		".menu a span.flex-1",
@@ -35,43 +12,47 @@ export default async function(page: Page) {
 		"h2"
 	]
 	const allInnerTextSelectors = innerTextSelectors.join(", ")
-	const allInnerTexts = page.locator(`css=${allInnerTextSelectors}`).allInnerTexts()
-
-	const allAlternateElements = page.locator(`css=[alt]`).all()
-
-	const allPendingResults: Promise<any>[] = []
-
-	const allPendingMetaElements = (await Promise.all(allMetaTextGroups)).flat().map(({
-		selector,
-		locator
-	}) => locator.then(elements => ({ selector, elements })))
-
-	await allInnerTexts.then(texts => {
-		allPendingResults.push(...checkEachGrammar(texts))
-	})
-	const allPendingRawMetaTexts = await Promise.all(allPendingMetaElements.map(pendingElement => {
-		return pendingElement.then(elementInfo => {
-			return elementInfo.elements.map(
-				element => element.getAttribute("content").then(
-					content => elementInfo.selector.includes("keywords")
+	const allAlternateTexts = page.locator(`css=[alt]`)
+		.all()
+		.then(elements => elements.map(element => {
+			element.getAttribute("alt")
+		}))
+	const metaSelectors = [
+		"description",
+		"keywords",
+		'"og:title"',
+		'"og:image:alt"',
+		'"og:description"'
+	]
+	const allMetaSelectors = metaSelectors.map(name => `meta[name=${name}]`)
+	const allMetaTexts = allMetaSelectors.map(
+		selector => page
+			.locator(selector)
+			.all()
+			.then(metas => metas.map(
+				meta => meta.getAttribute("content")
+				.then(
+					content => selector.includes("keywords")
 						? content.replace(/,/g, ", ")
-						: content
-				)
+						: content))
 			)
-		})
-	}))
-	const allPendingAlternateTexts = await allAlternateElements.then(
-		elements => elements.map(
-			element => element.getAttribute("alt")
-		)
 	)
-	const allPendingMetaTexts = await Promise.all(allPendingRawMetaTexts.flat())
-	allPendingResults.push(...checkEachGrammar(allPendingMetaTexts))
-	await Promise.all(allPendingAlternateTexts.flat()).then(texts => {
-		allPendingResults.push(...checkEachGrammar(texts))
+	const allTexts = (await Promise.all([
+		...(await Promise.all(allMetaTexts)).flat(),
+		...await allAlternateTexts,
+		page.locator(`css=${allInnerTextSelectors}`).allInnerTexts()
+	])).flat()
+
+	const pendingResults: Promise<any>[] = allTexts.map(async text => {
+		const result = await check(text, {
+			"api_url": "http://localhost:8081/v2/check",
+			dictionary
+		})
+
+		return result.matches
 	})
 
-	const expectedMatches = allPendingResults.map(() => [])
-	const matches = await Promise.all(allPendingResults)
+	const expectedMatches = allTexts.map(() => [])
+	const matches = await Promise.all(pendingResults)
 	await expect(matches).toEqual(expectedMatches)
 }
