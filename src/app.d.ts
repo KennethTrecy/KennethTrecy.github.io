d49493f0c42fcc8c52e30f421e3e9cfafd21aa37
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-13 10:35:43 +0800

    chore(environment)!: remove unused environment variable

--- src/app.d.ts
+++ src/app.d.ts
@@ -7,7 +7,6 @@ declare namespace App {
 	// interface PageData {}
 	interface Platform {
 		env: {
-			CF_PAGES_URL: string
 			PUBLIC_PRODUCTION_BASE_URL: string
 			PUBLIC_RECOMMENDED_DESCRIPTION_LENGTH: string
 			PUBLIC_MINIMUM_TITLE_LENGTH: string
