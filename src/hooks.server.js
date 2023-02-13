a8c6ae5345c63eb976728e43bc99c16334a9211b
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-13 19:43:34 +0800

    fix(server): limit the replacement of comma in body

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -3,24 +3,26 @@ import { dev } from "$app/environment"
 /** @type {import("@sveltejs/kit").Handle} */
 export async function handle({ event, resolve }) {
 	const response = await resolve(event, {
-		transformPageChunk: ({ html }) => {
+		"transformPageChunk": ({ html }) => {
 			if (dev) {
 				return html
 			}
 
-			const minimizeHTML = html.replace(/(\t|\r?\n)/g, "")
+			const minimizeHTML = html.replace(/(?<extraSpaces>\t|\r?\n)/gu, "")
 			const spacedHTML = minimizeHTML
-				// There are two cases that handled by the regex below:
-				// - Sentences after period start with uppercase letter or number
-				// - An acronym inside a sentence followed by a lowercase letter
-				.replace(/>\.([A-Za-z0-9])/g, ">. $1")
+				/*
+				 * There are two cases that handled by the regex below:
+				 * - Sentences after period start with uppercase letter or number
+				 * - An acronym inside a sentence followed by a lowercase letter
+				 */
+				.replace(/>\.(?<textAfterElement>[A-Za-z0-9])/gu, ">. $1")
 				// Handles the case of opening tags inside a sentence.
-				.replace(/([a-zA-Z0-9]{2,})<([a-z]+)/g, "$1 <$2")
+				.replace(/(?<textBeforeElement>[a-zA-Z0-9]{2,})<(?<elementName>[a-z]+)/gu, "$1 <$2")
 				// Ensure there is a space between commas.
-				.replace(/,([^ ])/g, ", $1")
+				.replace(/(?<=<body>),(?<contentAfterCommaInBody>[^ ])/gu, ", $1")
 			return spacedHTML
 		}
-	 })
+	})
 
 	return response
 }
