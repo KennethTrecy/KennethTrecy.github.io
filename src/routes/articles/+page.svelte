e6c521846b01592002c4a4b79f38fe311f0cedda
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-14 23:51:24 +0800

    feat(article): make the description of the page

--- src/routes/articles/+page.svelte
+++ src/routes/articles/+page.svelte
@@ -1,28 +1,25 @@
 <script lang="ts">
 	import pageMeta from "@/routes/articles/meta"
 
-	import CommonHead from "@/components/general/common_head.svelte"
-	import PrimaryHeading from "@/components/general/headings/primary.svelte"
-	import PageDetailCard from "@/components/general/independent_page_detail_card.svelte"
-	import StructuredArticle from "@/components/general/containers/structured_article.svelte"
+	import articleMetaCollection from "@/constants/article_meta_collection"
+
+	import defineHeadingInfo from "@/components/general/define_heading_info"
+	import SimpleText from "@/components/general/containers/simple_text.svelte"
+	import SecondaryHeading from "@/components/general/headings/secondary.svelte"
+	import ArticlePost from "@/components/general/containers/article_post.svelte"
+	import StructuredSection from "@/components/general/containers/structured_section.svelte"
+
+	const articlesWritten = defineHeadingInfo({
+		"prefix": "✏️",
+		"text": "Articles Written"
+	})
 </script>
 
-<svelte:head>
-	<CommonHead {pageMeta}/>
-</svelte:head>
-
-<StructuredArticle>
-	<PrimaryHeading slot="title">{pageMeta.title}</PrimaryHeading>
-	<svelte:fragment slot="content">
-		<div
-			itemprop="mainEntity"
-			itemscope
-			itemtype=""
-			class="">
-			<section class="">
-
-			</section>
-		</div>
-	</svelte:fragment>
-	<PageDetailCard slot="metadata" {pageMeta}/>
-</StructuredArticle>
+<ArticlePost {pageMeta}>
+	<StructuredSection itemprop="about" id={articlesWritten.id}>
+		<SecondaryHeading headingInfo={articlesWritten}/>
+		<SimpleText itemprop="mainEntity">
+			For some time, Kenneth Trecy may write articles and posts that may be useful for beginners and fellow developers. People are free to reference or link the articles below if they deemed informational or related to the topic they are in.
+		</SimpleText>
+	</StructuredSection>
+</ArticlePost>
