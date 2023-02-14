03a431f5cecbc8c0ecc8f078167454ce90f6f383
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-14 23:00:29 +0800

    internal(server): restore previous mechanism of reduction of spaces

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -8,12 +8,20 @@ export async function handle({ event, resolve }) {
 				return html
 			}
 
-			const minimizeHTML = html
-				.replace(/(?<extraSpaces>\t|\r)/gu, "")
-				.replace(/\n(?<closingTagAfterNewLine><\/[a-z]+>)/gu, "$1")
-				.replace(/\n+/gu, "\n")
-
-			return minimizeHTML
+			const minimizeHTML = html.replace(/(?<extraSpaces>\t|\r?\n)/gu, "")
+			const spacedHTML = minimizeHTML
+				/*
+				 * There are two cases that handled by the regex below:
+				 * - Sentences after period start with uppercase letter or number
+				 * - An acronym inside a sentence followed by a lowercase letter
+				 */
+				.replace(/>\.(?<textAfterElement>[A-Za-z0-9])/gu, ">. $1")
+				// Handles the case of opening tags inside a sentence.
+				.replace(/(?<textBeforeElement>[a-zA-Z0-9]{2,})<(?<elementName>[a-z]+)/gu, "$1 <$2")
+				// Ensure there is a space between commas.
+				.replace(/,(?<openingTag><\w+)/gu, ", $1")
+				.replace(/<\/span><span/gu, "</span> <span")
+			return spacedHTML
 		}
 	})
 
