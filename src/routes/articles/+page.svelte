2014d1f05007fb5e0ccce8fd625ec6dc55d9f0e1
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-15 00:02:01 +0800

    feat(article): list the articles
    The layout was inspired from project list.

--- src/routes/articles/+page.svelte
+++ src/routes/articles/+page.svelte
@@ -3,11 +3,14 @@
 
 	import articleMetaCollection from "@/constants/article_meta_collection"
 
+	import ArticleCard from "@/components/general/article_card.svelte"
 	import defineHeadingInfo from "@/components/general/define_heading_info"
 	import SimpleText from "@/components/general/containers/simple_text.svelte"
 	import SecondaryHeading from "@/components/general/headings/secondary.svelte"
 	import ArticlePost from "@/components/general/containers/article_post.svelte"
+	import StructuredList from "@/components/general/containers/structured_list.svelte"
 	import StructuredSection from "@/components/general/containers/structured_section.svelte"
+	import StructuredListItem from "@/components/general/containers/structured_list_item.svelte"
 
 	const articlesWritten = defineHeadingInfo({
 		"prefix": "✏️",
@@ -21,5 +24,15 @@
 		<SimpleText itemprop="mainEntity">
 			For some time, Kenneth Trecy may write articles and posts that may be useful for beginners and fellow developers. People are free to reference or link the articles below if they deemed informational or related to the topic they are in.
 		</SimpleText>
+		<StructuredList
+			order="descending"
+			isProjectList={false}
+			class={[ "list-none", "flex", "flex-col", "flex-wrap" ]}>
+			{#each articleMetaCollection as articleMeta}
+				<StructuredListItem isInProjectList={false}>
+					<ArticleCard {articleMeta}/>
+				</StructuredListItem>
+			{/each}
+		</StructuredList>
 	</StructuredSection>
 </ArticlePost>
