bf266af813dd440c58bcb1d4edcae6d9025d0938
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-15 19:41:16 +0800

    dprct(type): deprecate the types in body

--- src/types/body.ts
+++ src/types/body.ts
@@ -1,3 +1,12 @@
+/**
+ * @module
+ * Do not add here anymore. If there are still files refererring to the types in this module, move
+ * them to `./container_info.ts`
+ */
+
+/**
+ * @deprecated since v2.2.0. Use the type in container info instead.
+ */
 export type HeadingInfo<T extends "defined"|"raw" = "raw"> = {
 	prefix?: string
 	text: string
@@ -9,6 +18,9 @@ export type HeadingInfo<T extends "defined"|"raw" = "raw"> = {
 		: never
 )
 
+/**
+ * @deprecated since v2.2.0. Use the type in container info instead.
+ */
 export type Order =
 	| "ascending"
 	| "descending"
