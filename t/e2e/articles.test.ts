3a07ddc2ad2c02efb0ee844ee594119812d020fa
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-15 19:27:20 +0800

    test(e2e): make test for the articles

---
+++ t/e2e/articles.test.ts
@@ -0,0 +1,9 @@
+import { test } from "@playwright/test"
+
+import checkGrammar from "../utilities/check_grammar.ts"
+
+test("page has correct grammar", async ({ page }) => {
+	await page.goto("/articles")
+
+	await checkGrammar(page)
+})
