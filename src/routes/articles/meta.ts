e8b50a8609dca32749d8dab57e0f11f0e523f5da
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-15 19:28:29 +0800

    fix(article): correct the path to articles

--- src/routes/articles/meta.ts
+++ src/routes/articles/meta.ts
@@ -3,7 +3,7 @@ import articleMetaCollection from "@/constants/article_meta_collection"
 import definePageMeta from "@/components/general/define_page_meta"
 
 const publishedDateTimes = articleMetaCollection.map(meta => meta.datePublished.getTime())
-const meta = definePageMeta("/", {
+const meta = definePageMeta("/articles", {
 	"datePublished": new Date(Math.min(...publishedDateTimes)),
 	"dateModified": new Date(Math.max(...publishedDateTimes)),
 	"description": "Collection of articles written by Kenneth Trecy",
