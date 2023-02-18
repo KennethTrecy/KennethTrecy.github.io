a5d72d663b17e43eac297afbe006f6d1b5544ae9
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-18 17:52:53 +0800

    fix(article): correct the type of the article

--- src/routes/articles/different_levels_of_abstraction_in_software/meta.ts
+++ src/routes/articles/different_levels_of_abstraction_in_software/meta.ts
@@ -2,10 +2,11 @@ import definePageMeta from "@/components/general/define_page_meta"
 
 const meta = definePageMeta("/articles/different_levels_of_abstraction_in_software", {
 	"datePublished": new Date(Date.UTC(2023, 1, 18, 3, 40)),
+	"dateModified": new Date(Date.UTC(2023, 1, 18, 10, 15)),
 	"description": "Find out different levels of abstraction in software and their differences.",
 	"keywords": [ "Abstraction", "level", "software", "customizability", "reusability" ],
 	"title": "Different Levels of Abstraction in Software",
-	"version": "1.0"
+	"version": "1.001-dev"
 })
 
 export default meta
