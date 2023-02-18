25bba35bea9103fbb385122341b9fbaa0b292034
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-18 23:29:28 +0800

    feat(article): write initial draft

--- src/routes/articles/centering_a_child_element/+page.svelte
+++ src/routes/articles/centering_a_child_element/+page.svelte
@@ -24,7 +24,7 @@
 	<StructuredSection itemprop="about" id={introduction.id}>
 		<SecondaryHeading headingInfo={introduction}/>
 		<SimpleText itemprop="about">
-
+			Centering an element is a usual task for a web developer. Images, modals, navigation anchor, and container of block of texts are the common components desired to be in the center. Semantically, Cascading Style Sheets (<abbr>CSS</abbr>) uses the word <em itemprop="keyword">center<em> to indicate that an element has equal spaces with respect to intended axis. It may center a content according to its x-axis (e.g. <code>text-align: center;</code>), y-axis (e.g. <code>vertical-align: middle;</code>), or both. This article explores common ways to put an element in the center and presents a unorthodox way for a special use case.
 		</SimpleText>
 	</StructuredSection>
 </ArticlePost>
