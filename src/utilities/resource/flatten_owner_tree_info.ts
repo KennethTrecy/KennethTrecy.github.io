0096b2816ab4ec55f8fe73f043e645a2f6b9d7b8
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-18 20:05:04 +0800

    dprct(utility): move utility for flattening tree in a directory

---
+++ src/utilities/resource/flatten_owner_tree_info.ts
@@ -0,0 +1,22 @@
+import type { ViewableOwnerInfoTree, CompleteViewableFileInfo } from "@/types/container_info"
+
+export default function(forest: ViewableOwnerInfoTree[]): CompleteViewableFileInfo[] {
+	return forest.reduce((previousFileInfo, currentOwner) => [
+		...previousFileInfo,
+		...currentOwner.repos.reduce((previousRepos, currentRepo) => [
+			...previousRepos,
+			...currentRepo.branches.reduce((previousBranches, currentBranch) => [
+				...previousBranches,
+				...currentBranch.paths.reduce((previousPaths, currentPath) => [
+					...previousPaths,
+					{
+						"owner": currentOwner.owner,
+						"repo": currentRepo.repo,
+						"branch": currentBranch.branch,
+						"path": currentPath
+					}
+				], <CompleteViewableFileInfo[]>[])
+			], <CompleteViewableFileInfo[]>[])
+		], <CompleteViewableFileInfo[]>[])
+	], <CompleteViewableFileInfo[]>[])
+}
