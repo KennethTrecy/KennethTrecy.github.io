bfc4fa30e98d8752738169b94ef46718dfcf0549
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-18 23:31:29 +0800

    seo(article): correct the metadata of the article

--- src/routes/articles/centering_a_child_element/meta.ts
+++ src/routes/articles/centering_a_child_element/meta.ts
@@ -1,6 +1,6 @@
-import definePageMeta from "@/utilities/definers/define_page_meta"
+import defineArticlePageMeta from "@/utilities/definers/define_article_page_meta"
 
-const meta = definePageMeta("/articles/different_levels_of_abstraction_in_software", {
+const meta = defineArticlePageMeta("/articles/centering_a_child_element", {
 	"datePublished": new Date(Date.UTC(2023, 2, 20)),
 	"description": "Looking for ways to center a <div> element? Find them out in this page.",
 	"keywords": [ "CSS", "layout tutorial", "centering" ],
