14523dfefc165a91335cb5ad0c18db8dc151d8c4
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-19 00:18:17 +0800

    refactor(article): separate the component for indicating keywords

---
+++ src/components/general/containers/keyword.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let otherItemprop: string = ""
+
+	export { otherItemprop as itemprop }
+
+	$: itemprop = otherItemprop === "" ? "keywords" : `${otherItemprop} keywords`
+</script>
+
+<strong {itemprop}><slot></slot></strong>
