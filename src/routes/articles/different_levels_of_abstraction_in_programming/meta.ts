7aba5383fe2aca56777f07e2e09ab3be21020ca4
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-19 23:19:11 +0800

    seo(article): change the description and keywords

--- src/routes/articles/different_levels_of_abstraction_in_programming/meta.ts
+++ src/routes/articles/different_levels_of_abstraction_in_programming/meta.ts
@@ -3,7 +3,7 @@ import defineArticlePageMeta from "@/utilities/definers/define_article_page_meta
 const meta = defineArticlePageMeta("/articles/different_levels_of_abstraction_in_programming", {
 	"datePublished": new Date(Date.UTC(2023, 1, 18, 3, 40)),
 	"dateModified": new Date(Date.UTC(2023, 1, 19, 4, 0)),
-	"description": "Find out different levels of abstraction in software and their differences.",
+	"description": "Find out different levels of programming in software and their differences.",
 	"keywords": [
 		"Abstraction",
 		"level",
