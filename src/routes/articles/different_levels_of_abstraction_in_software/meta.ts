702c42562ab7a5368e38df0a21faba64f74ac839
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-19 23:49:48 +0800

    seo(article): add new keywords

--- src/routes/articles/different_levels_of_abstraction_in_software/meta.ts
+++ src/routes/articles/different_levels_of_abstraction_in_software/meta.ts
@@ -4,7 +4,14 @@ const meta = defineArticlePageMeta("/articles/different_levels_of_abstraction_in
 	"datePublished": new Date(Date.UTC(2023, 1, 18, 3, 40)),
 	"dateModified": new Date(Date.UTC(2023, 1, 18, 10, 15)),
 	"description": "Find out different levels of abstraction in software and their differences.",
-	"keywords": [ "Abstraction", "level", "software", "customizability", "reusability" ],
+	"keywords": [
+		"Abstraction",
+		"level",
+		"software development",
+		"customizability",
+		"reusability",
+		"programming"
+	],
 	"title": "Different Levels of Abstraction in Software",
 	"version": "1.002-dev"
 })
