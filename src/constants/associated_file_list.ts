91bbe8997a14e368ca08e26c9095403581dd2884
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-19 23:16:42 +0800

    feat(article): rename the article title

--- src/constants/associated_file_list.ts
+++ src/constants/associated_file_list.ts
@@ -2,7 +2,7 @@ import type { CompleteViewableFileInfo } from "@/types/container_info"
 
 import {
 	associatedFileList as fileList20220218
-} from "@/routes/articles/different_levels_of_abstraction_in_software/shared_constants"
+} from "@/routes/articles/different_levels_of_abstraction_in_programming/shared_constants"
 
 const generalAssociatedFileList: CompleteViewableFileInfo[] = [
 	...fileList20220218
