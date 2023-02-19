3f06864cdaa0a5bd7fceff1e570ce9cd5be456b7
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-19 10:25:31 +0800

    refactor(article): utilize component for keywords

--- src/routes/articles/centering_a_child_element/+page.svelte
+++ src/routes/articles/centering_a_child_element/+page.svelte
@@ -6,6 +6,7 @@
 	import pageMeta from "@/routes/articles/centering_a_child_element/meta"
 
 	import Bookmark from "@/components/general/links/bookmark.svelte"
+	import Keyword from "@/components/general/containers/keyword.svelte"
 	import ExternalLink from "@/components/general/links/external.svelte"
 	import defineHeadingInfo from "@/components/general/define_heading_info"
 	import SimpleText from "@/components/general/containers/simple_text.svelte"
@@ -28,10 +29,10 @@
 	<StructuredSection itemprop="about" id={introduction.id}>
 		<SecondaryHeading headingInfo={introduction}/>
 		<SimpleText itemprop="about">
-			<strong itemprop="keywords">Centering</strong> an element is a usual task for a web developer. Images, modals, navigation anchor, and container of block of texts are the common components desired to be in the center. Semantically, Cascading Style Sheets (<abbr>CSS</abbr>) uses the word <em>center</em> to indicate that an element has equal spaces with respect to intended axis.
+			<Keyword>Centering</Keyword> an element is a usual task for a web developer. Images, modals, navigation anchor, and container of block of texts are the common components desired to be in the center. Semantically, Cascading Style Sheets (<abbr>CSS</abbr>) uses the word <em>center</em> to indicate that an element has equal spaces with respect to intended axis.
 		</SimpleText>
 		<SimpleText>
-			A programmer may center a content according to its <strong itemprop="keywords">x-axis</strong> (e.g. <code>text-align: center;</code>), <strong itemprop="keywords">y-axis</strong> (e.g. <code>vertical-align: middle;</code>), or both. This article explores common ways to put an element in the center and presents a unorthodox way for a special use case.
+			A programmer may center a content according to its <Keyword>x-axis</Keyword> (e.g. <code>text-align: center;</code>), <Keyword>y-axis</Keyword> (e.g. <code>vertical-align: middle;</code>), or both. This article explores common ways to put an element in the center and presents a unorthodox way for a special use case.
 		</SimpleText>
 	</StructuredSection>
 </ArticlePost>
