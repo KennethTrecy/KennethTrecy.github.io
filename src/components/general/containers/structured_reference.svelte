d459a85eaab2fec64c5293e6b87b3e62c01c0911
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-20 15:42:47 +0800

    internal(component): complete the structure of reference

--- src/components/general/containers/structured_reference.svelte
+++ src/components/general/containers/structured_reference.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
+	import { referenceInfos } from "@/components/general/containers/reference_info_collection.ts"
+
 	import defineHeadingInfo from "@/utilities/definers/define_heading_info"
 
 	import SecondaryHeading from "@/components/general/headings/secondary.svelte"
 	import StructuredList from "@/components/general/containers/structured_list.svelte"
 	import StructuredSection from "@/components/general/containers/structured_section.svelte"
 	import StructuredListItem from "@/components/general/containers/structured_list_item.svelte"
-
+	import ReferenceLink from "@/components/general/containers/structured_reference/link.svelte"
 
 	const references = defineHeadingInfo({
 		"text": "References"
@@ -15,7 +17,10 @@
 <StructuredSection id={references.id}>
 	<SecondaryHeading headingInfo={references}>
 	<StructuredList order="ascending">
-		<StructuredListItem>
-		</StructuredListItem>
+		{#each $referenceInfos as referenceInfo}
+			<StructuredListItem>
+				<ReferenceLink {info}/>
+			</StructuredListItem>
+		{/each}
 	</StructuredList>
 </StructuredSection>
