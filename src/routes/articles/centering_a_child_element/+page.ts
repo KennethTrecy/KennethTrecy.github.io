4b90a446d610a39ec6140e1097ce394587271c17
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-20 10:16:53 +0800

    internal(article): load associated files
    The mechanism is almost the same from first article but uses different
    associated file list.

---
+++ src/routes/articles/centering_a_child_element/+page.ts
@@ -0,0 +1,31 @@
+import type { CodeFile } from "@/types/container_info"
+import type {
+	PageLoad
+} from "@/routes/articles/centering_a_child_element/$types"
+
+import {
+	associatedFileList
+} from "@/routes/articles/centering_a_child_element/shared_constants"
+
+export const load: PageLoad = async({ fetch }) => {
+	const pendingExtractedFileInfoList: Promise<CodeFile>[] = []
+
+	for (const fileInfo of associatedFileList) {
+		const repoNamespace = `${fileInfo.owner}/${fileInfo.repo}`
+		const fileNamespace = `${fileInfo.branch}/${fileInfo.path}`
+		const URL = `/api/v0/github/${repoNamespace}/code/${fileNamespace}`
+		pendingExtractedFileInfoList.push(
+			fetch(URL, { "method": "GET" })
+				.then(response => response.json())
+				.then(rawCodeInfo => rawCodeInfo as CodeFile)
+				.then(codeFile => ({
+					...codeFile,
+					...fileInfo
+				}))
+		)
+	}
+
+	return {
+		"loadedFileInfos": await Promise.all(pendingExtractedFileInfoList)
+	}
+}
