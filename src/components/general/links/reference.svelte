bf12d8315755a4ae08106c95956566f70407a770
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-20 14:35:34 +0800

    internal(component): make initial reference component

---
+++ src/components/general/links/reference.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { onMount } from "svelte"
+
+	import type { ReferenceInfo } from "@/types/reference"
+
+	import { PUBLIC_PRODUCTION_BASE_URL } from "$env/static/public"
+
+	import { appendReference } from "@/components/general/containers/reference_info_collection.ts"
+
+	import BaseLink from "@/components/general/links/base.svelte"
+	import ExternalLink from "@/components/general/links/external.svelte"
+
+	export let info: ReferenceInfo
+
+	onMount(async () => {
+		appendReference(info)
+	})
+
+	$: isInbound = info.link.includes(PUBLIC_PRODUCTION_BASE_URL)
+</script>
