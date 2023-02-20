46a993519e8f0b00c767cb5aa4f97d750e9ffae9
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-20 13:33:06 +0800

    internal(component): make initial structure of the reference

---
+++ src/components/general/containers/references.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import StructuredSection from "@/components/general/containers/structured_section.svelte"
+
+	import defineHeadingInfo from "@/utilities/definers/define_heading_info"
+	import SecondaryHeading from "@/components/general/headings/secondary.svelte"
+	import StructuredList from "@/components/general/containers/structured_list.svelte"
+	import StructuredListItem from "@/components/general/containers/structured_list_item.svelte"
+
+	const references = defineHeadingInfo({
+		"text": "References"
+	})
+</script>
+
+<StructuredSection id={references.id}>
+	<SecondaryHeading headingInfo={references}>
+	<StructuredList order="ascending">
+		<StructuredListItem>
+		</StructuredListItem>
+	</StructuredList>
+</StructuredSection>
