085b60710cd33d1c8f02a94a51d574c925c914bf
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-20 10:38:41 +0800

    internal(constant): add associated file list of another article

--- src/constants/associated_file_list.ts
+++ src/constants/associated_file_list.ts
@@ -3,9 +3,13 @@ import type { CompleteViewableFileInfo } from "@/types/container_info"
 import {
 	associatedFileList as fileList20220218
 } from "@/routes/articles/different_levels_of_abstraction_in_software/shared_constants"
+import {
+	associatedFileList as fileList20220220
+} from "@/routes/articles/centering_a_child_element/shared_constants"
 
 const generalAssociatedFileList: CompleteViewableFileInfo[] = [
-	...fileList20220218
+	...fileList20220218,
+	...fileList20220220
 ]
 
 export default generalAssociatedFileList
