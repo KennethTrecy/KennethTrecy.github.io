e12e42e816113bb1815bc08c402bf2c9fb637f4d
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-20 14:45:12 +0800

    internal(component): make component for citation

--- src/components/general/containers/structured_reference.svelte
+++ src/components/general/containers/structured_reference.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	import StructuredSection from "@/components/general/containers/structured_section.svelte"
-
 	import defineHeadingInfo from "@/utilities/definers/define_heading_info"
+
 	import SecondaryHeading from "@/components/general/headings/secondary.svelte"
 	import StructuredList from "@/components/general/containers/structured_list.svelte"
+	import StructuredSection from "@/components/general/containers/structured_section.svelte"
 	import StructuredListItem from "@/components/general/containers/structured_list_item.svelte"
+
 
 	const references = defineHeadingInfo({
 		"text": "References"
---
+++ src/components/general/links/external_citation.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { onMount } from "svelte"
+
+	import type { ReferenceInfo } from "@/types/reference"
+
+	import { appendReference } from "@/components/general/containers/reference_info_collection.ts"
+
+	import ExternalLink from "@/components/general/links/external.svelte"
+
+	export let info: ReferenceInfo
+	export let itemprop: string|undefined = undefined
+
+	onMount(async () => {
+		appendReference(info)
+	})
+</script>
+
+<ExternalLink address={info.link} {itemprop}><slot></slot></ExternalLink>
