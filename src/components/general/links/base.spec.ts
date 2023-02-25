0f5adf178bacb49f73253561d2b6d51f020965f4
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-25 19:29:33 +0800

    test(unit): ensure base link may have scope if required

---
+++ src/components/general/links/base.spec.ts
@@ -0,0 +1,43 @@
+// @vitest-environment jsdom
+
+import type { AnchorLinkType } from "@/types/container_info"
+
+import { cleanup, render } from "@testing-library/svelte"
+import { describe, expect, it } from "vitest"
+
+import Component from "./base.svelte"
+
+describe("Base link behavior", () => {
+	it("can have no scope", () => {
+		const address = "https://example.com/own_scope"
+		const relationship: AnchorLinkType[] = [ "external" ]
+		const { container } = render(Component, {
+			address,
+			relationship
+		})
+
+		const property = container.querySelector("[itemscope]")
+
+		expect(property).toBeNull()
+
+		cleanup()
+	})
+
+	it("can have own scope", () => {
+		const itemtype = "https://schema.org/CreativeWork"
+		const address = "https://example.com/own_scope"
+		const relationship: AnchorLinkType[] = [ "external" ]
+		const { container } = render(Component, {
+			itemtype,
+			address,
+			relationship
+		})
+
+		const property = container.querySelector("[itemscope]")
+
+		expect(property).not.toBeNull()
+		expect(property?.getAttribute("rel")).toContain("external")
+
+		cleanup()
+	})
+})
