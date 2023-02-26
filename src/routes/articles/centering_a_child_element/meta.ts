ae5efe7728723d68fa4c2e904a0e98a0f95db193
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-02-26 18:33:41 +0800

    seo(article): update version and modified date

--- src/routes/articles/centering_a_child_element/meta.ts
+++ src/routes/articles/centering_a_child_element/meta.ts
@@ -2,11 +2,12 @@ import defineArticlePageMeta from "@/utilities/definers/define_article_page_meta
 
 const meta = defineArticlePageMeta("/articles/centering_a_child_element", {
 	"datePublished": new Date(Date.UTC(2023, 1, 26, 9, 40)),
+	"dateModified": new Date(Date.UTC(2023, 1, 26, 10, 40)),
 	// eslint-disable-next-line max-len
 	"description": "Looking for ways to center an element? Smaller or larger than its parent? Learn about them in this article.",
 	"keywords": [ "CSS", "layout tutorial", "centering" ],
 	"title": "Centering a Child Element",
-	"version": "1.001-dev",
+	"version": "1.001",
 	"objectType": "article"
 })
 
