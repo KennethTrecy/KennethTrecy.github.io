cbdafa1ca41fe696ffc7c79f53c057218b77a756
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-03-08 22:34:26 +0800

    internal(component): allow addition of relationship to be consistent
    External link component and base link component are both used in bound
    link component. For consistency, both components should have similar
    attributes.

--- src/components/general/links/external.svelte
+++ src/components/general/links/external.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import type { AnchorLinkType } from "@/types/container_info"
+
 	import { externalTypes } from "@/components/general/links/constants"
 
 	import Link from "@/components/general/links/base.svelte"
@@ -6,8 +8,16 @@
 	export let address: string
 	export let itemprop: string|undefined = undefined
 	export let itemtype: string|undefined = undefined
+	export let relationship: AnchorLinkType|AnchorLinkType[] = []
+
+	$: relationshipTypes = [
+		...externalTypes,
+		...Array.isArray(relationship)
+			? relationship
+			: [ relationship ]
+	]
 </script>
 
-<Link address={address} relationship={externalTypes} context="other" {itemprop} {itemtype}>
+<Link address={address} relationship={relationshipTypes} context="other" {itemprop} {itemtype}>
 	<slot></slot>
 </Link>
