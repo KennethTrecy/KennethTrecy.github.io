60160c1d25398e34e3de82e5d96b51680354cc94
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-03-11 23:54:01 +0800

    internal(component): make quaternary heading

---
+++ src/components/general/headings/quaternary.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { HeadingInfo, SubheadingVariant } from "@/types/container_info"
+	import Subheading from "@/components/general/headings/subheading.svelte"
+
+	export let headingInfo: HeadingInfo<"defined">
+	export let variant: SubheadingVariant = "headline"
+	export let mayUseBookmark: boolean = true
+	let otherClasses: string[] = []
+
+	export { otherClasses as class }
+
+	$: joinedClasses = [
+		...otherClasses
+	]
+</script>
+
+<Subheading level={4} {headingInfo} {variant} {mayUseBookmark} class={joinedClasses}>
+	<slot></slot>
+</Subheading>
