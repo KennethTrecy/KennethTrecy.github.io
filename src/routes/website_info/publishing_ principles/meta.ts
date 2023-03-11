e87a338fa15428a4f95362364bd62f5fe366c350
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-03-11 16:16:44 +0800

    seo(website info): add metadata for publishing principles

---
+++ src/routes/website_info/publishing_ principles/meta.ts
@@ -0,0 +1,12 @@
+import definePageMeta from "@/utilities/definers/define_page_meta"
+
+const meta = definePageMeta("/website_info/publishing_principles", {
+	"datePublished": new Date(Date.UTC(2023, 2, 13, 0, 0)),
+	// eslint-disable-next-line max-len
+	"description": "Publishing principles that should be adhered by this website have been listed in this page.",
+	"keywords": [ "publishing", "principles", "publishing guide" ],
+	"title": "Publishing Principles of the Website",
+	"version": "0.1-dev"
+})
+
+export default meta
