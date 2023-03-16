baaa3cf005fc5c678a719c698f25ef693a13ca2a
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-03-17 00:36:56 +0800

    seo: finalize version and timestamps

--- src/routes/articles/centering_a_child_element/meta.ts
+++ src/routes/articles/centering_a_child_element/meta.ts
@@ -2,12 +2,12 @@ import defineArticlePageMeta from "@/utilities/definers/define_article_page_meta
 
 const meta = defineArticlePageMeta("/articles/centering_a_child_element", {
 	"datePublished": new Date(Date.UTC(2023, 1, 26, 9, 40)),
-	"dateModified": new Date(Date.UTC(2023, 2, 9, 4, 30)),
+	"dateModified": new Date(Date.UTC(2023, 2, 16, 16, 45)),
 	// eslint-disable-next-line max-len
 	"description": "Looking for ways to center an element? Smaller or larger than its parent? Learn about them in this article.",
 	"keywords": [ "CSS", "layout tutorial", "centering" ],
 	"title": "Centering a Child Element",
-	"version": "1.003-dev",
+	"version": "1.003",
 	"objectType": "article",
 	"articleType": "technical article"
 })
--- src/routes/website_info/publishing_principles/meta.ts
+++ src/routes/website_info/publishing_principles/meta.ts
@@ -3,12 +3,12 @@ import { principlesPath } from "@/constants/schema_collection"
 import definePageMeta from "@/utilities/definers/define_page_meta"
 
 const meta = definePageMeta(principlesPath, {
-	"datePublished": new Date(Date.UTC(2023, 2, 13, 0, 0)),
+	"datePublished": new Date(Date.UTC(2023, 2, 16, 16, 45)),
 	// eslint-disable-next-line max-len
 	"description": "Publishing principles that should be adhered by this website have been listed in this page.",
 	"keywords": [ "publishing", "principles", "publishing guide" ],
 	"title": "Publishing Principles of the Website",
-	"version": "0.1-dev"
+	"version": "1.0"
 })
 
 export default meta
