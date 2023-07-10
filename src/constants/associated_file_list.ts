855e13d8a8f495eabbb7b714fba14fe0d81817d8
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-10 16:12:49 +0800

    internal(constant): add new file list to permit

--- src/constants/associated_file_list.ts
+++ src/constants/associated_file_list.ts
@@ -6,10 +6,14 @@ import {
 import {
 	associatedFileList as fileList20220220
 } from "@/routes/articles/centering_a_child_element/shared_constants"
+import {
+	associatedFileList as fileList20230710
+} from "@/routes/articles/key_steps_to_add_gramma_for_improved_ci/shared_constants"
 
 const generalAssociatedFileList: CompleteViewableFileInfo[] = [
 	...fileList20220218,
-	...fileList20220220
+	...fileList20220220,
+	...fileList20230710
 ]
 
 export default generalAssociatedFileList
