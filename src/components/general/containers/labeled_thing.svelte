80c9a17a024bdbf016369e354884204893cce2fc
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-10 21:43:53 +0800

    internal(component): make a component for a labeled item

---
+++ src/components/general/containers/labeled_thing.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let label: string
+</script>
+
+<span itemprop="item" itemscope itemtype="https://schema.org/Thing">
+	<strong itemprop="name">{label}</strong>.
+	<span itemprop="description"><slot></slot></span>
+</span>
