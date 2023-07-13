efc9f65f19c64bb79645b7a1c40a9895d7a9c66c
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-13 22:32:19 +0800

    chore(article): update metadata

--- src/routes/articles/meta.ts
+++ src/routes/articles/meta.ts
@@ -5,11 +5,11 @@ import definePageMeta from "@/utilities/definers/define_page_meta"
 const publishedDateTimes = articleMetaCollection.map(meta => meta.datePublished.getTime())
 const meta = definePageMeta("/articles", {
 	"datePublished": new Date(Math.min(...publishedDateTimes)),
-	"dateModified": new Date(Math.max(...publishedDateTimes)),
+	"dateModified": new Date(Math.max(...publishedDateTimes, Date.UTC(2023, 7, 14, 8, 0, 0))),
 	"description": "Collection of articles written by Kenneth Trecy",
 	"keywords": [ "Kenneth Trecy", "articles", "collection" ],
 	"title": "Kenneth Trecy's Article Collection",
-	"version": "1.002"
+	"version": "1.003-dev"
 })
 
 export default meta
