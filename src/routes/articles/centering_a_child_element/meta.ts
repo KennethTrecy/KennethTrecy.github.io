b635c9de23fbc53d799c3df6cd0ff86136bc0b48
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-14 16:25:32 +0800

    fix(article): correct dates

--- src/routes/articles/centering_a_child_element/meta.ts
+++ src/routes/articles/centering_a_child_element/meta.ts
@@ -2,7 +2,7 @@ import defineArticlePageMeta from "@/utilities/definers/define_article_page_meta
 
 const meta = defineArticlePageMeta("/articles/centering_a_child_element", {
 	"datePublished": new Date(Date.UTC(2023, 1, 26, 9, 40)),
-	"dateModified": new Date(Date.UTC(2023, 7, 13, 14, 15)),
+	"dateModified": new Date(Date.UTC(2023, 6, 13, 14, 15)),
 	// eslint-disable-next-line max-len
 	"description": "Looking for ways to center an element? Smaller or larger than its parent? Learn about them in this article.",
 	"keywords": [ "CSS", "layout tutorial", "centering" ],
