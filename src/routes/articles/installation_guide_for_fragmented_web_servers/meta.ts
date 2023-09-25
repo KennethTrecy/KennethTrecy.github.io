33fc3bfe7c066e8990ff1b4823dfbde8a82908ca
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-09-25 20:26:27 +0800

    internal(article): prepare metadata of new page

---
+++ src/routes/articles/installation_guide_for_fragmented_web_servers/meta.ts
@@ -0,0 +1,19 @@
+import defineArticlePageMeta from "@/utilities/definers/define_article_page_meta"
+
+const meta = defineArticlePageMeta("/articles/installation_guide_for_fragmented_web_servers", {
+	"datePublished": new Date(Date.UTC(2023, 8, 30, 0, 0)),
+	"description": "Guide to install Apache HTTP server, PHP, MySQL, and others separately for your development environment.",
+	"keywords": [
+		"instruction",
+		"guide",
+		"server",
+		"installation",
+		"developer"
+	],
+	"title": "Installation Guide for Fragmented Web Servers",
+	"version": "0.1-dev",
+	"objectType": "article",
+	"articleType": "article"
+})
+
+export default meta
