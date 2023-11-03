ffd919647915872d26311ac1233f6abb3ab5759b
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-11-03 22:14:42 +0800

    internal(project): prepare the updated metadata

--- src/routes/projects/meta.ts
+++ src/routes/projects/meta.ts
@@ -4,12 +4,12 @@ import definePageMeta from "@/utilities/definers/define_page_meta"
 
 const meta = definePageMeta("/projects", {
 	"datePublished": FIRST_PUBLICATION_DATE,
-	"dateModified": new Date(Date.UTC(2023, 1, 13, 11, 30)),
+	"dateModified": new Date(Date.UTC(2023, 10, 4, 11, 30)),
 	// eslint-disable-next-line max-len
 	"description": "Kenneth Trecy has a decade of experience in programming websites. Here is an overview of different projects he was involved.",
 	"keywords": [ "Kenneth Trecy", "involved", "personal", "projects" ],
 	"title": "List of Involved Projects",
-	"version": "1.001"
+	"version": "1.002-dev"
 })
 
 export default meta
