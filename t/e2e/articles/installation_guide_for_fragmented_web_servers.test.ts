067f34320a401ab1982d0f58e4eb9c5f49dc9f8d
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-11-05 20:05:29 +0800

    test(e2e): check the grammar of new articles

--- t/e2e/articles/installation_guide_for_fragmented_web_servers.test.ts
+++ t/e2e/articles/installation_guide_for_fragmented_web_servers.test.ts
@@ -3,7 +3,7 @@ import { test } from "@playwright/test"
 import checkGrammar from "../../utilities/check_grammar.ts"
 
 test("page has correct grammar", async ({ page }) => {
-	await page.goto("/articles/key_steps_to_add_gramma_for_improved_ci")
+	await page.goto("/articles/installation_guide_for_fragmented_web_servers")
 
 	await checkGrammar(page)
 })
