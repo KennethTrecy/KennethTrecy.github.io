cf5296288e46c4b31eb116e0773a67e005345204
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-11-05 20:25:44 +0800

    internal(article): prepare updated metadata of improved articles

--- src/routes/articles/key_steps_to_add_gramma_for_improved_ci/meta.ts
+++ src/routes/articles/key_steps_to_add_gramma_for_improved_ci/meta.ts
@@ -2,11 +2,12 @@ import defineArticlePageMeta from "@/utilities/definers/define_article_page_meta
 
 const meta = defineArticlePageMeta("/articles/key_steps_to_add_gramma_for_improved_ci", {
 	"datePublished": new Date(Date.UTC(2023, 6, 13, 14, 15)),
+	"datePublished": new Date(Date.UTC(2023, 10, 5, 13, 30)),
 	// eslint-disable-next-line max-len
 	"description": "Learn how to automatically proofread your website's page content by integrating Gramma in CI.",
 	"keywords": [ "Gramma", "proofread", "continuous integration" ],
 	"title": "Improved CI: Key Steps to Add Gramma",
-	"version": "1.0",
+	"version": "1.001-dev",
 	"objectType": "article",
 	"articleType": "technical article"
 })
