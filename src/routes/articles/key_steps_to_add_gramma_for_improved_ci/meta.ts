b6aaf16e76bb5081a6a5527c66c6d61f83661dea
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-11-05 21:32:10 +0800

    fix(article): correct dates

--- src/routes/articles/key_steps_to_add_gramma_for_improved_ci/meta.ts
+++ src/routes/articles/key_steps_to_add_gramma_for_improved_ci/meta.ts
@@ -2,7 +2,7 @@ import defineArticlePageMeta from "@/utilities/definers/define_article_page_meta
 
 const meta = defineArticlePageMeta("/articles/key_steps_to_add_gramma_for_improved_ci", {
 	"datePublished": new Date(Date.UTC(2023, 6, 13, 14, 15)),
-	"dateModified": new Date(Date.UTC(2023, 10, 5, 13, 30)),
+	"dateModified": new Date(Date.UTC(2023, 10, 5, 14, 0)),
 	// eslint-disable-next-line max-len
 	"description": "Learn how to automatically proofread your website's page content by integrating Gramma in CI.",
 	"keywords": [ "Gramma", "proofread", "continuous integration" ],
--- src/routes/projects/meta.ts
+++ src/routes/projects/meta.ts
@@ -4,7 +4,7 @@ import definePageMeta from "@/utilities/definers/define_page_meta"
 
 const meta = definePageMeta("/projects", {
 	"datePublished": FIRST_PUBLICATION_DATE,
-	"dateModified": new Date(Date.UTC(2023, 10, 4, 11, 30)),
+	"dateModified": new Date(Date.UTC(2023, 10, 5, 14, 0)),
 	// eslint-disable-next-line max-len
 	"description": "Kenneth Trecy has a decade of experience in programming websites. Here is an overview of different projects he was involved.",
 	"keywords": [ "Kenneth Trecy", "involved", "personal", "projects" ],
