a2f438576e208f52ff968c8f15bd52b3ccae8488
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-03-31 08:53:18 +0800

    fix: remove outdated URL

--- src/constants/redirected_meta_collection.ts
+++ src/constants/redirected_meta_collection.ts
@@ -1,10 +1,6 @@
 import { type PageMeta } from "@/types/content_metadata"
 
-import article20220218 from "@/routes/articles/different_levels_of_abstraction_in_software/meta"
-
 // These collection will be kept to allow the moved URL to be crawled.
-const metaCollection = <PageMeta[]>[
-	article20220218
-]
+const metaCollection = <PageMeta[]>[]
 
 export default metaCollection
