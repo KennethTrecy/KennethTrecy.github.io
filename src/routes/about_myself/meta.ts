f68b8fe03c8ad07b416b9cf0b4d4c3885837733c
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-03-31 11:32:18 +0800

    internal(about myself): prepare updating the page

--- src/routes/about_myself/meta.ts
+++ src/routes/about_myself/meta.ts
@@ -3,12 +3,12 @@ import definePageMeta from "@/utilities/definers/define_page_meta"
 
 const meta = definePageMeta("/about_myself", {
 	"datePublished": FIRST_PUBLICATION_DATE,
-	"dateModified": new Date(Date.UTC(2023, 1, 15, 3, 10)),
+	"dateModified": new Date(Date.UTC(2024, 3, 31, 3, 0)),
 	// eslint-disable-next-line max-len
 	"description": "Know more about Kenneth Trecy's journey and his skills on I.T. field. Contact details have been listed here too if you want to connect with him.",
 	"keywords": [ "Kenneth Trecy", "contact", "journey", "skills", "Filipino programmer" ],
 	"title": "Who am I?: Journey of Trecy",
-	"version": "1.002"
+	"version": "1.003-dev"
 })
 
 export default meta
